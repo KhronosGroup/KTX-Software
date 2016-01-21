1f7f82e3eb2c21dae7ac7df7146968fdfa0a191d
msc- <github@callow.im>
2016-01-21 10:03:31 +0900

    Add .bat file to initialize environment for actions.

---
+++ setup_env.bat
@@ -0,0 +1,7 @@
+REM Set up to use Git Bash for running "cygwin" actions.
+
+REM CD to where this file lives to avoid path relativiation problems.
+cd %~dp0
+
+set PATH=%PATH%;C:\Program Files\Git\bin
+
