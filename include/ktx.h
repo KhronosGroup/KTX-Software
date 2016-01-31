7950962cdee17e776398f68f7ed71f48da58dcee
Your Name <you@example.com>
2016-01-31 23:26:23 +1000

    Modifications to several files to allow for the use of GL3W instead of GLEW. Added KHR/khrplatform.h from [1] as it was not include by default.
    [1] http://www.opensource.apple.com/source/X11libs/X11libs-60/mesa/Mesa-7.8.2/include/KHR/khrplatform.h?txt

--- include/ktx.h
+++ include/ktx.h
@@ -137,9 +137,15 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 	#ifdef _WIN32
 	  #include <windows.h>
-	  #include <GL/glew.h>
+      #ifdef KTX_USE_GL3W_GETPROC
+	    #include <GL/gl3w.h>
+      #else
+        #include <GL/glew.h>
+      #endif
     #elif KTX_USE_GETPROC
       #include <GL/glew.h>
+    #elif KTX_USE_GL3W_GETPROC
+      #include <GL/gl3w.h>
     #else
       #define GLCOREARB_PROTOTYPES
       #include <GL/glcorearb.h>
