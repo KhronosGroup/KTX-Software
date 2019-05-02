47b9cf6c72835f78dd21011cbd8abc707806b47b
MarkCallow <github@callow.im>
2019-05-02 12:24:44 -0700

    Add copyright/license and do not edit warning.

--- lib/dfdutils/dfd.h
+++ lib/dfdutils/dfd.h
@@ -1,7 +1,8 @@
-/*! \file */
+/* -*- tab-width: 4; -*- */
+/* vi: set sw=2 ts=4 expandtab: */
 
 /*
- * Copyright (c) 2019 The Khronos Group Inc.
+ * Copyright (c) 2010-2018 The Khronos Group Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +15,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
+
+/**
+ * @internal
+ * @file dfd.h
+ * @~English
+ *
+ * @brief Header file for the data format descriptor Utilities.
  */
 
 /*
