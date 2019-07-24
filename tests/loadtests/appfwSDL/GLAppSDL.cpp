ac4a33f202d055d09d15240c15edea6098c679fb
MarkCallow <github@callow.im>
2019-07-23 22:12:54 -0700

    Don't request sRGB FB for ES1.

--- tests/loadtests/appfwSDL/GLAppSDL.cpp
+++ tests/loadtests/appfwSDL/GLAppSDL.cpp
@@ -59,10 +59,11 @@ GLAppSDL::initialize(int argc, char* argv[])
         return false;
 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, profile);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3/*majorVersion*/);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, majorVersion);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, minorVersion);
 #if !defined(EMSCRIPTEN)
-    SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
+    if (majorVersion >= 3)
+      SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
 #endif
 #if defined(DEBUG) && !defined(EMSCRIPTEN)
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
