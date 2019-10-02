46f853aa7cde4b1d281e713a2a787cefe2addeba
MarkCallow <github@callow.im>
2019-10-02 18:38:53 +0900

    Add back std::endl.

--- tools/toktx/toktx.cpp
+++ tools/toktx/toktx.cpp
@@ -762,8 +762,7 @@ int _tmain(int argc, _TCHAR* argv[])
                         delete srcImg;
                         std::cerr << appName
                                   << ": PNG file has ICC profile chunk. "
-                                  << "These are not supported.";
-                        //          << std::endl;
+                                  << "These are not supported." << std::endl;
                         exitCode = 1;
                         goto cleanup;
                     } else if (state.info_png.gama_defined) {
