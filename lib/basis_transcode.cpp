078bd0d679537c4544f64675fa6cfb4ccaa9c6f0
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-10-03 15:16:10 +0200

    Fixed KTX_TF_RGBA4444 transcoding with alpha channel

--- lib/basis_transcode.cpp
+++ lib/basis_transcode.cpp
@@ -687,21 +687,22 @@ ktxTexture2_TranscodeBasis(ktxTexture2* This,
 #if !BASISD_SUPPORT_UNCOMPRESSED
               return KTX_UNSUPPORTED_FEATURE;
 #endif
+                if(hasAlpha) {
+                    status = llt.transcode_slice(writePtr, num_blocks_x, num_blocks_y,
+                        basisData + levelOffset + sliceDescs[image].alphaSliceByteOffset,
+                        sliceDescs[image].alphaSliceByteLength,
+                        basist::block_format::cRGBA4444_ALPHA, bytes_per_block,
+                        (transcodeFlags & KTX_DF_BC1_FORBID_THREE_COLOR_BLOCKS) == 0,
+                        isVideo, hasAlpha, 0/* level_index*/, width, height );
+                }
+
+                if (status) {
                 status = llt.transcode_slice(writePtr, num_blocks_x, num_blocks_y,
                         basisData + levelOffset + sliceByteOffset, sliceByteLength,
                         hasAlpha ? basist::block_format::cRGBA4444_COLOR : basist::block_format::cRGBA4444_COLOR_OPAQUE,
                         bytes_per_block,
                         (transcodeFlags & KTX_DF_BC1_FORBID_THREE_COLOR_BLOCKS) == 0,
-                        isVideo, hasAlpha, 0/* level_index*/, width, height );
-
-                if (status) {
-                    if(hasAlpha) {
-                        status = llt.transcode_slice(writePtr, num_blocks_x, num_blocks_y,
-                            basisData + levelOffset + sliceByteOffset, sliceByteLength,
-                            basist::block_format::cRGBA4444_ALPHA, bytes_per_block,
-                            (transcodeFlags & KTX_DF_BC1_FORBID_THREE_COLOR_BLOCKS) == 0,
-                            isVideo, hasAlpha, 0/* level_index*/, width, height );
-                    }
+                        isVideo, false, 0/* level_index*/, width, height );
                 }
                 if (!status) {
                      result = KTX_TRANSCODE_FAILED;
