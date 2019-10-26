6505bfac390d44920bebf26d48f96883c0b26e42
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-10-26 04:03:41 +0200

    compiler fix: regionsArrayEnd is not available in non _DEBUG builds.

--- lib/vkloader.c
+++ lib/vkloader.c
@@ -298,7 +298,9 @@ optimalTilingCallback(int miplevel, int face,
     user_cbdata_optimal* ud = (user_cbdata_optimal*)userdata;
 
     // Set up copy to destination region in final image
+#if defined(_DEBUG)
     assert(ud->region < ud->regionsArrayEnd);
+#endif
     ud->region->bufferOffset = ud->offset;
     ud->offset += faceLodSize;
     // These 2 are expressed in texels.
@@ -357,7 +359,9 @@ optimalTilingPadCallback(int miplevel, int face,
     ktx_uint32_t rowPitch = width * ud->elementSize;
 
     // Set bufferOffset in destination region in final image
+#if defined(_DEBUG)
     assert(ud->region < ud->regionsArrayEnd);
+#endif
     ud->region->bufferOffset = ud->offset;
 
     // Copy data into staging buffer
