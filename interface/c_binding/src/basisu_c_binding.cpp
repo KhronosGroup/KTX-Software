4c49849f08b77d1543e9f06d750d4b5e363b433e
Andreas Atteneder <andreas.atteneder@gmail.com>
2019-10-26 05:31:55 +0200

    Refactor

--- interface/c_binding/src/basisu_c_binding.cpp
+++ interface/c_binding/src/basisu_c_binding.cpp
@@ -21,215 +21,203 @@
 
 #include <basisu_transcoder.h>
 
+#include "basisu_c_binding.h"
+
 using namespace basist;
 
-basist::etc1_global_selector_codebook const *g_pGlobal_codebook;
+basist::etc1_global_selector_codebook *g_pGlobal_codebook;
 
 #define MAGIC 0xDEADBEE1
 
-class basis_file
-{
-    int m_magic = 0;
-    basisu_transcoder m_transcoder;
-    const uint8_t *m_file;
-    size_t byteLength;
-    
-public:
-    basis_file()
-    :
-    m_transcoder(g_pGlobal_codebook)
-    {}
-    
-    bool open(const uint8_t *buffer, size_t newByteLength) {
-        m_file = buffer;
-        byteLength = newByteLength;
-    
-        if (!m_transcoder.validate_header(buffer, newByteLength)) {
-            m_file = nullptr;
-            byteLength = 0;
-            return false;
-        }
-        
-        // Initialized after validation
-        m_magic = MAGIC;
-        return true;
-    }
-    
-    void close() {
-        assert(m_magic == MAGIC);
+bool basis_file::open(const uint8_t *buffer, size_t newByteLength) {
+    m_file = buffer;
+    byteLength = newByteLength;
+
+    if (!m_transcoder.validate_header(buffer, newByteLength)) {
         m_file = nullptr;
         byteLength = 0;
+        return false;
     }
     
-    uint32_t getHasAlpha() {
-        assert(m_magic == MAGIC);
-        if (m_magic != MAGIC)
-            return 0;
-        
-        basisu_image_level_info li;
-        if (!m_transcoder.get_image_level_info(m_file, byteLength, li, 0, 0))
-            return 0;
-        
-        return li.m_alpha_flag;
-    }
-    
-    uint32_t getNumImages() {
-        assert(m_magic == MAGIC);
-        if (m_magic != MAGIC)
-            return 0;
-        
-        return m_transcoder.get_total_images(m_file, byteLength);
-    }
-    
-    uint32_t getNumLevels(uint32_t image_index) {
-        assert(m_magic == MAGIC);
-        if (m_magic != MAGIC)
-            return 0;
-        
-        basisu_image_info ii;
-        if (!m_transcoder.get_image_info(m_file, byteLength, ii, image_index))
-            return 0;
-        
-        return ii.m_total_levels;
-    }
-    
-    uint32_t getImageWidth(uint32_t image_index, uint32_t level_index) {
-        assert(m_magic == MAGIC);
-        if (m_magic != MAGIC)
-            return 0;
-        
-        uint32_t orig_width, orig_height, total_blocks;
-        if (!m_transcoder.get_image_level_desc(m_file, byteLength, image_index, level_index, orig_width, orig_height, total_blocks))
-            return 0;
-        
-        return orig_width;
-    }
-    
-    uint32_t getImageHeight(uint32_t image_index, uint32_t level_index) {
-        assert(m_magic == MAGIC);
-        if (m_magic != MAGIC)
-            return 0;
-        
-        uint32_t orig_width, orig_height, total_blocks;
-        if (!m_transcoder.get_image_level_desc(m_file, byteLength, image_index, level_index, orig_width, orig_height, total_blocks))
-            return 0;
-        
-        return orig_height;
-    }
-    
-    uint32_t getImageTranscodedSizeInBytes(uint32_t image_index, uint32_t level_index, uint32_t format) {
-        assert(m_magic == MAGIC);
-        if (m_magic != MAGIC)
-            return 0;
-        
-        if (format >= (uint32_t) basist::transcoder_texture_format::cTFTotalTextureFormats)
-            return 0;
-        
-        uint32_t orig_width, orig_height, total_blocks;
-        if (!m_transcoder.get_image_level_desc(m_file, byteLength, image_index, level_index, orig_width, orig_height, total_blocks))
-            return 0;
+    // Initialized after validation
+    m_magic = MAGIC;
+    return true;
+}
 
-        const transcoder_texture_format transcoder_format = static_cast<transcoder_texture_format>(format);
+void basis_file::close() {
+    assert(m_magic == MAGIC);
+    m_file = nullptr;
+    byteLength = 0;
+}
 
-        if (basis_transcoder_format_is_uncompressed(transcoder_format))
+uint32_t basis_file::getHasAlpha() {
+    assert(m_magic == MAGIC);
+    if (m_magic != MAGIC)
+        return 0;
+    
+    basisu_image_level_info li;
+    if (!m_transcoder.get_image_level_info(m_file, byteLength, li, 0, 0))
+        return 0;
+    
+    return li.m_alpha_flag;
+}
+
+uint32_t basis_file::getNumImages() {
+    assert(m_magic == MAGIC);
+    if (m_magic != MAGIC)
+        return 0;
+    
+    return m_transcoder.get_total_images(m_file, byteLength);
+}
+
+uint32_t basis_file::getNumLevels(uint32_t image_index) {
+    assert(m_magic == MAGIC);
+    if (m_magic != MAGIC)
+        return 0;
+    
+    basisu_image_info ii;
+    if (!m_transcoder.get_image_info(m_file, byteLength, ii, image_index))
+        return 0;
+    
+    return ii.m_total_levels;
+}
+
+uint32_t basis_file::getImageWidth(uint32_t image_index, uint32_t level_index) {
+    assert(m_magic == MAGIC);
+    if (m_magic != MAGIC)
+        return 0;
+    
+    uint32_t orig_width, orig_height, total_blocks;
+    if (!m_transcoder.get_image_level_desc(m_file, byteLength, image_index, level_index, orig_width, orig_height, total_blocks))
+        return 0;
+    
+    return orig_width;
+}
+
+uint32_t basis_file::getImageHeight(uint32_t image_index, uint32_t level_index) {
+    assert(m_magic == MAGIC);
+    if (m_magic != MAGIC)
+        return 0;
+    
+    uint32_t orig_width, orig_height, total_blocks;
+    if (!m_transcoder.get_image_level_desc(m_file, byteLength, image_index, level_index, orig_width, orig_height, total_blocks))
+        return 0;
+    
+    return orig_height;
+}
+
+uint32_t basis_file::getImageTranscodedSizeInBytes(uint32_t image_index, uint32_t level_index, uint32_t format) {
+    assert(m_magic == MAGIC);
+    if (m_magic != MAGIC)
+        return 0;
+    
+    if (format >= (uint32_t) basist::transcoder_texture_format::cTFTotalTextureFormats)
+        return 0;
+    
+    uint32_t orig_width, orig_height, total_blocks;
+    if (!m_transcoder.get_image_level_desc(m_file, byteLength, image_index, level_index, orig_width, orig_height, total_blocks))
+        return 0;
+
+    const transcoder_texture_format transcoder_format = static_cast<transcoder_texture_format>(format);
+
+    if (basis_transcoder_format_is_uncompressed(transcoder_format))
+    {
+        // Uncompressed formats are just plain raster images.
+        const uint32_t bytes_per_pixel = basis_get_uncompressed_bytes_per_pixel(transcoder_format);
+        const uint32_t bytes_per_line = orig_width * bytes_per_pixel;
+        const uint32_t bytes_per_slice = bytes_per_line * orig_height;
+        return bytes_per_slice;
+    }
+    else
+    {
+        // Compressed formats are 2D arrays of blocks.
+        const uint32_t bytes_per_block = basis_get_bytes_per_block(transcoder_format);
+
+        if (transcoder_format == transcoder_texture_format::cTFPVRTC1_4_RGB || transcoder_format == transcoder_texture_format::cTFPVRTC1_4_RGBA)
         {
-            // Uncompressed formats are just plain raster images.
-            const uint32_t bytes_per_pixel = basis_get_uncompressed_bytes_per_pixel(transcoder_format);
-            const uint32_t bytes_per_line = orig_width * bytes_per_pixel;
-            const uint32_t bytes_per_slice = bytes_per_line * orig_height;
-            return bytes_per_slice;
-        }
-        else
-        {
-            // Compressed formats are 2D arrays of blocks.
-            const uint32_t bytes_per_block = basis_get_bytes_per_block(transcoder_format);
-
-            if (transcoder_format == transcoder_texture_format::cTFPVRTC1_4_RGB || transcoder_format == transcoder_texture_format::cTFPVRTC1_4_RGBA)
-            {
-                // For PVRTC1, Basis only writes (or requires) total_blocks * bytes_per_block. But GL requires extra padding for very small textures: 
-                // https://www.khronos.org/registry/OpenGL/extensions/IMG/IMG_texture_compression_pvrtc.txt
-                const uint32_t width = (orig_width + 3) & ~3;
-                const uint32_t height = (orig_height + 3) & ~3;
-                const uint32_t size_in_bytes = (std::max(8U, width) * std::max(8U, height) * 4 + 7) / 8;
-                return size_in_bytes;
-            }
-
-            return total_blocks * bytes_per_block;
-        }
-    }
-    
-    uint32_t startTranscoding() {
-        assert(m_magic == MAGIC);
-        if (m_magic != MAGIC)
-            return 0;
-        
-        return m_transcoder.start_transcoding(m_file, byteLength);
-    }
-    
-    uint32_t transcodeImage(void* dst, size_t dst_size, uint32_t image_index, uint32_t level_index, uint32_t format, uint32_t pvrtc_wrap_addressing, uint32_t get_alpha_for_opaque_formats) {
-        assert(m_magic == MAGIC);
-        if (m_magic != MAGIC)
-            return 0;
-        
-        if (format >= (uint32_t) basist::transcoder_texture_format::cTFTotalTextureFormats)
-            return 0;
-        
-        const transcoder_texture_format transcoder_format = static_cast<transcoder_texture_format>(format);
-
-        uint32_t orig_width, orig_height, total_blocks;
-        if (!m_transcoder.get_image_level_desc(m_file, byteLength, image_index, level_index, orig_width, orig_height, total_blocks))
-            return 0;
-
-        uint32_t flags = get_alpha_for_opaque_formats ? basisu_transcoder::cDecodeFlagsTranscodeAlphaDataToOpaqueFormats : 0;
-
-        uint32_t status;
-
-        if (basis_transcoder_format_is_uncompressed(transcoder_format))
-        {
-            const uint32_t bytes_per_pixel = basis_get_uncompressed_bytes_per_pixel(transcoder_format);
-            const uint32_t bytes_per_line = orig_width * bytes_per_pixel;
-            const uint32_t bytes_per_slice = bytes_per_line * orig_height;
-
-            assert(bytes_per_slice <= dst_size);
-
-            status = m_transcoder.transcode_image_level(
-                m_file, byteLength, image_index, level_index,
-                dst, orig_width * orig_height,
-                transcoder_format,
-                flags,
-                orig_width,
-                nullptr,
-                orig_height);
-        }
-        else
-        {
-            uint32_t bytes_per_block = basis_get_bytes_per_block(transcoder_format);
-
-            uint32_t required_size = total_blocks * bytes_per_block;
-
-            if (transcoder_format == transcoder_texture_format::cTFPVRTC1_4_RGB || transcoder_format == transcoder_texture_format::cTFPVRTC1_4_RGBA)
-            {
-                // For PVRTC1, Basis only writes (or requires) total_blocks * bytes_per_block. But GL requires extra padding for very small textures: 
-                // https://www.khronos.org/registry/OpenGL/extensions/IMG/IMG_texture_compression_pvrtc.txt
-                // The transcoder will clear the extra bytes followed the used blocks to 0.
-                const uint32_t width = (orig_width + 3) & ~3;
-                const uint32_t height = (orig_height + 3) & ~3;
-                required_size = (std::max(8U, width) * std::max(8U, height) * 4 + 7) / 8;
-                assert(required_size >= total_blocks * bytes_per_block);
-            }
-
-            assert(required_size <= dst_size);
-
-            status = m_transcoder.transcode_image_level(
-                m_file, byteLength, image_index, level_index,
-                dst, dst_size / bytes_per_block,
-                static_cast<basist::transcoder_texture_format>(format),
-                flags);
+            // For PVRTC1, Basis only writes (or requires) total_blocks * bytes_per_block. But GL requires extra padding for very small textures: 
+            // https://www.khronos.org/registry/OpenGL/extensions/IMG/IMG_texture_compression_pvrtc.txt
+            const uint32_t width = (orig_width + 3) & ~3;
+            const uint32_t height = (orig_height + 3) & ~3;
+            const uint32_t size_in_bytes = (std::max(8U, width) * std::max(8U, height) * 4 + 7) / 8;
+            return size_in_bytes;
         }
 
-        return status;
+        return total_blocks * bytes_per_block;
     }
-};
+}
+
+uint32_t basis_file::startTranscoding() {
+    assert(m_magic == MAGIC);
+    if (m_magic != MAGIC)
+        return 0;
+    
+    return m_transcoder.start_transcoding(m_file, byteLength);
+}
+
+uint32_t basis_file::transcodeImage(void* dst, size_t dst_size, uint32_t image_index, uint32_t level_index, uint32_t format, uint32_t pvrtc_wrap_addressing, uint32_t get_alpha_for_opaque_formats) {
+    assert(m_magic == MAGIC);
+    if (m_magic != MAGIC)
+        return 0;
+    
+    if (format >= (uint32_t) basist::transcoder_texture_format::cTFTotalTextureFormats)
+        return 0;
+    
+    const transcoder_texture_format transcoder_format = static_cast<transcoder_texture_format>(format);
+
+    uint32_t orig_width, orig_height, total_blocks;
+    if (!m_transcoder.get_image_level_desc(m_file, byteLength, image_index, level_index, orig_width, orig_height, total_blocks))
+        return 0;
+
+    uint32_t flags = get_alpha_for_opaque_formats ? basisu_transcoder::cDecodeFlagsTranscodeAlphaDataToOpaqueFormats : 0;
+
+    uint32_t status;
+
+    if (basis_transcoder_format_is_uncompressed(transcoder_format))
+    {
+        const uint32_t bytes_per_pixel = basis_get_uncompressed_bytes_per_pixel(transcoder_format);
+        const uint32_t bytes_per_line = orig_width * bytes_per_pixel;
+        const uint32_t bytes_per_slice = bytes_per_line * orig_height;
+
+        assert(bytes_per_slice <= dst_size);
+
+        status = m_transcoder.transcode_image_level(
+            m_file, byteLength, image_index, level_index,
+            dst, orig_width * orig_height,
+            transcoder_format,
+            flags,
+            orig_width,
+            nullptr,
+            orig_height);
+    }
+    else
+    {
+        uint32_t bytes_per_block = basis_get_bytes_per_block(transcoder_format);
+
+        uint32_t required_size = total_blocks * bytes_per_block;
+
+        if (transcoder_format == transcoder_texture_format::cTFPVRTC1_4_RGB || transcoder_format == transcoder_texture_format::cTFPVRTC1_4_RGBA)
+        {
+            // For PVRTC1, Basis only writes (or requires) total_blocks * bytes_per_block. But GL requires extra padding for very small textures: 
+            // https://www.khronos.org/registry/OpenGL/extensions/IMG/IMG_texture_compression_pvrtc.txt
+            // The transcoder will clear the extra bytes followed the used blocks to 0.
+            const uint32_t width = (orig_width + 3) & ~3;
+            const uint32_t height = (orig_height + 3) & ~3;
+            required_size = (std::max(8U, width) * std::max(8U, height) * 4 + 7) / 8;
+            assert(required_size >= total_blocks * bytes_per_block);
+        }
+
+        assert(required_size <= dst_size);
+
+        status = m_transcoder.transcode_image_level(
+            m_file, byteLength, image_index, level_index,
+            dst, dst_size / bytes_per_block,
+            static_cast<basist::transcoder_texture_format>(format),
+            flags);
+    }
+
+    return status;
+}
 
 #ifdef KTX_BASISU_C_BINDINGS
 
