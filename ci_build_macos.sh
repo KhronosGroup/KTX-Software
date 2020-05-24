f9d05bd7af0dfa8626670aa9d4ebdde91bb6df37
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-05-24 22:15:06 +0200

    [CI] chore: moved install/build scripts to dedicated files.

---
+++ ci_build_macos.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# exit if any command fails
+set -e
+
+# Due to the spaces in the platform names, must use array variables so
+# destination args can be expanded to a single word.
+OSX_XCODE_OPTIONS=(-alltargets -destination "platform=OS X,arch=x86_64")
+IOS_XCODE_OPTIONS=(-alltargets -destination "generic/platform=iOS" -destination "platform=iOS Simulator,OS=latest")
+XCODE_CODESIGN_ENV='CODE_SIGN_IDENTITY= CODE_SIGN_ENTITLEMENTS= CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO'
+# The following and more is needed if you want to actually sign the code.
+# See http://stackoverflow.com/questions/27671854/travis-ci-fails-to-build-with-a-code-signing-error.
+#KEY_CHAIN=xcode-build.keychain
+#security create-keychain -p travis $KEY_CHAIN
+# Make the keychain the default so identities are found
+#security default-keychain -s $KEY_CHAIN
+## Unlock the keychain
+#security unlock-keychain -p travis $KEY_CHAIN
+## Set keychain locking timeout to 3600 seconds
+#security set-keychain-settings -t 3600 -u $KEY_CHAIN
+
+# Ensure that Vulkan SDK's glslc is in PATH
+export PATH="${VULKAN_SDK}/bin:$PATH"
+
+echo "Configure KTX-Software (macOS)"
+cmake -GXcode -Bbuild-macos -DKTX_FEATURE_DOC=ON
+echo "Build KTX-Software (macOS Debug)"
+cmake --build build-macos --config Debug
+cmake --build build-macos --config Debug --target RUN_TESTS
+echo "Build KTX-Software (macOS Release)"
+cmake --build build-macos --config Release
+cmake --build build-macos --config Release --target RUN_TESTS
+echo "Configure KTX-Software (iOS)"
+cmake -GXcode -Bbuild-ios -DCMAKE_SYSTEM_NAME=iOS -DVULKAN_SDK="${VULKAN_INSTALL_DIR}/MoltenVK"
+echo "Build KTX-Software (iOS Debug)"
+cmake --build build-ios --config Debug
+echo "Build KTX-Software (iOS Release)"
+cmake --build build-ios --config Release
