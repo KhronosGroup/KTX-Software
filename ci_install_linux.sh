d99b3bee9be25cd54ba040d5092f70cb5ad8c9ce
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-05-24 22:15:06 +0200

    [CI] chore: moved install/build scripts to dedicated files.

---
+++ ci_install_linux.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# exit if any command fails
+set -e
+
+sudo apt-get -qq update
+sudo apt-get -qq install libzstd-dev
+sudo apt-get -qq install ninja-build
+sudo apt-get -qq install doxygen
+sudo apt-get -qq install libsdl2-dev
+sudo apt-get -qq install libgl1-mesa-glx libgl1-mesa-dev
+sudo apt-get -qq install libvulkan1 libvulkan-dev
+sudo apt-get -qq install libassimp4 libassimp-dev
+wget -qO - http://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-$VULKAN_SDK_VER-bionic.list http://packages.lunarg.com/vulkan/$VULKAN_SDK_VER/lunarg-vulkan-$VULKAN_SDK_VER-bionic.list
+sudo apt update
+sudo apt install lunarg-vulkan-sdk
