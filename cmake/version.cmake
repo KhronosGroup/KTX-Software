c77d4d0b78d8deb5e6ae918b89f6f5d597c34a5c
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-05-29 18:42:53 +0200

    [CI] fix: create version.h header file for tools as well

---
+++ cmake/version.cmake
@@ -0,0 +1,32 @@
+#
+# Create a version.h header file using the mkversion shell script
+#
+
+function( create_version_header dest_path target )
+
+    set( version_h_output ${dest_path}/version.h)
+
+    if(WIN32)
+        add_custom_command(
+            OUTPUT ${version_h_output}
+            COMMAND call call "setup_env.bat" && bash -c "\"./mkversion\" \"-o\" \"version.h\" \"${dest_path}\""
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+            COMMENT Generate ${version_h_output}
+            VERBATIM
+        )
+    else()
+        add_custom_command(
+            OUTPUT ${version_h_output}
+            COMMAND ./mkversion -o version.h ${dest_path}
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+            COMMENT Generate ${version_h_output}
+            VERBATIM
+        )
+    endif()
+
+    set( version_target ${target}_version )
+    add_custom_target( ${version_target} DEPENDS ${version_h_output} )
+    add_dependencies( ${target} ${version_target} )
+    # target_sources( ${version_target} PRIVATE ${version_h_output} )
+
+endfunction()
