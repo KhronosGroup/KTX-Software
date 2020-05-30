0a60d2dbe71148a55bb8671e57e664f6ba1b8db7
MarkCallow <github@callow.im>
2020-05-29 17:24:00 -0700

    Enable mouse touch events in SDL.

--- tests/loadtests/appfwSDL/GLAppSDL.cpp
+++ tests/loadtests/appfwSDL/GLAppSDL.cpp
@@ -96,6 +96,7 @@ GLAppSDL::initialize(int argc, char* argv[])
 
 #if __MACOSX__
     SDL_SetHint(SDL_HINT_MAC_CTRL_CLICK_EMULATE_RIGHT_CLICK, "1");
+    SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "1");
 #endif
     
     pswMainWindow = SDL_CreateWindow(
