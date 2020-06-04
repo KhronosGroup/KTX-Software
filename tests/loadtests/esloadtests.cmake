9e82f2c62b3223c73c7a81c7592f495ef8427a01
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-06-05 01:33:59 +0200

    [CMake] WIP version of es3loadtests working on iOS. - Proper Info.plist configuration/value replacement - Tried to remove redundant include dirs by usint target interface include dirs

--- tests/loadtests/esloadtests.cmake
+++ tests/loadtests/esloadtests.cmake
@@ -1,31 +1,19 @@
 
-add_executable( gl3loadtests
+add_executable( es3loadtests
     ${EXE_FLAG}
     $<TARGET_OBJECTS:appfwSDL>
     $<TARGET_OBJECTS:GLAppSDL>
     $<TARGET_OBJECTS:objUtil>
-    glloadtests/shader-based/BasisuTest.cpp
-    glloadtests/shader-based/BasisuTest.h
-    glloadtests/shader-based/DrawTexture.cpp
-    glloadtests/shader-based/DrawTexture.h
-    glloadtests/shader-based/GL3LoadTests.cpp
-    glloadtests/shader-based/GL3LoadTestSample.cpp
-    glloadtests/shader-based/GL3LoadTestSample.h
-    glloadtests/shader-based/mygl.h
-    glloadtests/shader-based/shaders.cpp
-    glloadtests/shader-based/TextureArray.cpp
-    glloadtests/shader-based/TextureArray.h
-    glloadtests/shader-based/TextureCubemap.cpp
-    glloadtests/shader-based/TextureCubemap.h
-    glloadtests/shader-based/TexturedCube.cpp
-    glloadtests/shader-based/TexturedCube.h
-    glloadtests/utils/GLMeshLoader.hpp
-    glloadtests/utils/GLTextureTranscoder.hpp
+    glloadtests/gles1/ES1LoadTests.cpp
+    glloadtests/gles1/DrawTexture.cpp
+    glloadtests/gles1/DrawTexture.h
+    glloadtests/gles1/TexturedCube.cpp
+    glloadtests/gles1/TexturedCube.h
     ${LOAD_TEST_COMMON_RESOURCE_FILES}
 )
 
 target_include_directories(
-    gl3loadtests
+    es3loadtests
 PRIVATE
     $<TARGET_PROPERTY:appfwSDL,INTERFACE_INCLUDE_DIRECTORIES>
     $<TARGET_PROPERTY:GLAppSDL,INTERFACE_INCLUDE_DIRECTORIES>
@@ -35,38 +23,67 @@ PRIVATE
 
 if(OPENGL_FOUND)
     target_include_directories(
-        gl3loadtests
+        es3loadtests
     PRIVATE
         ${OPENGL_INCLUDE_DIR}
     )
 endif()
 
 target_link_libraries(
-    gl3loadtests
+    es3loadtests
     ktx
 )
 
 if(OPENGL_FOUND AND NOT EMSCRIPTEN)
     target_link_libraries(
-        gl3loadtests
+        es3loadtests
         ${OPENGL_LIBRARIES}
     )
 endif()
 
 if(SDL2_FOUND)
     target_link_libraries(
-        gl3loadtests
+        es3loadtests
         ${SDL2_LIBRARIES}
     )
 endif()
 
 if(ZLIB_FOUND)
-    target_link_libraries( gl3loadtests ZLIB::ZLIB )
+    target_link_libraries( es3loadtests ZLIB::ZLIB )
 endif()
 
-if(EMSCRIPTEN)
+if(APPLE)
+    if(IOS)
+        set( INFO_PLIST "${PROJECT_SOURCE_DIR}/tests/loadtests/glloadtests/resources/ios/Info.plist" )
+        target_sources(
+            es3loadtests
+        PRIVATE
+            ${PROJECT_SOURCE_DIR}/icons/ios/CommonIcons.xcassets
+            glloadtests/resources/ios/LaunchImages.xcassets
+            glloadtests/resources/ios/LaunchScreen.storyboard
+        )
+
+        target_link_libraries(
+            es3loadtests
+            ${AudioToolbox_LIBRARY}
+            ${AVFoundation_LIBRARY}
+            ${CoreAudio_LIBRARY}
+            ${CoreBluetooth_LIBRARY}
+            ${CoreGraphics_LIBRARY}
+            ${CoreMotion_LIBRARY}
+            ${Foundation_LIBRARY}
+            ${GameController_LIBRARY}
+            ${Metal_LIBRARY}
+            ${OpenGLES_LIBRARY}
+            ${QuartzCore_LIBRARY}
+            ${UIKit_LIBRARY}
+        )
+    else()
+        set( INFO_PLIST "${PROJECT_SOURCE_DIR}/tests/loadtests/glloadtests/resources/mac/Info.plist" )
+    endif()
+elseif(EMSCRIPTEN)
     set_target_properties(
-        gl3loadtests
+        es3loadtests
     PROPERTIES
         COMPILE_FLAGS "-Wpedantic -s DISABLE_EXCEPTION_CATCHING=0 -s USE_SDL=2 -s USE_WEBGL2=1 -O0 -g"
         # LINK_FLAGS "--source-map-base ./ --preload-file testimages --exclude-file testimages/genref --exclude-file testimages/*.pgm --exclude-file testimages/*.ppm --exclude-file testimages/*.pam --exclude-file testimages/*.pspimage -s ALLOW_MEMORY_GROWTH=1 -s DISABLE_EXCEPTION_CATCHING=0 -s USE_SDL=2 -s USE_WEBGL2=1 -g4"
@@ -74,36 +91,35 @@ if(EMSCRIPTEN)
     )
 elseif(WIN32)
     target_sources(
-        gl3loadtests
+        es3loadtests
     PRIVATE
         glloadtests/resources/win/glloadtests.rc
         glloadtests/resources/win/resource.h
     )
     target_link_libraries(
-        gl3loadtests
+        es3loadtests
         "${CMAKE_SOURCE_DIR}/other_lib/win/Release-x64/glew32.lib"
     )
-    ensure_runtime_dependencies_windows(gl3loadtests)
+    ensure_runtime_dependencies_windows(es3loadtests)
 endif()
 
-target_link_libraries( gl3loadtests ${LOAD_TEST_COMMON_LIBS} )
+target_link_libraries( es3loadtests ${LOAD_TEST_COMMON_LIBS} )
 
 target_compile_definitions(
-    gl3loadtests
+    es3loadtests
 PRIVATE
     $<TARGET_PROPERTY:ktx,INTERFACE_COMPILE_DEFINITIONS>
-    "GL_CONTEXT_PROFILE=SDL_GL_CONTEXT_PROFILE_CORE"
+    "GL_CONTEXT_PROFILE=SDL_GL_CONTEXT_PROFILE_ES"
     GL_CONTEXT_MAJOR_VERSION=3
-    GL_CONTEXT_MINOR_VERSION=3
+    GL_CONTEXT_MINOR_VERSION=0
 )
 
 if(APPLE)
-    set(INFO_PLIST "${PROJECT_SOURCE_DIR}/tests/loadtests/glloadtests/resources/mac/Info.plist" )
-    set(PRODUCT_NAME "gl3loadtests")
+    set(PRODUCT_NAME "es3loadtests")
     set(EXECUTABLE_NAME ${PRODUCT_NAME})
     set(PRODUCT_BUNDLE_IDENTIFIER "org.khronos.ktx.${PRODUCT_NAME}")
     configure_file( ${INFO_PLIST} Info.plist )
-    set_target_properties( gl3loadtests PROPERTIES
+    set_target_properties( es3loadtests PROPERTIES
         MACOSX_BUNDLE_INFO_PLIST "${CMAKE_CURRENT_BINARY_DIR}/Info.plist"
         MACOSX_BUNDLE_ICON_FILE "ktx_app.icns"
         # Because libassimp is built with bitcode disabled. It's not important unless
@@ -113,10 +129,10 @@ if(APPLE)
     )
 endif()
 if(KTX_ICON)
-    set_target_properties( gl3loadtests PROPERTIES RESOURCE ${KTX_ICON} )
+    set_target_properties( es3loadtests PROPERTIES RESOURCE ${KTX_ICON} )
 endif()
 
 
 if(EMSCRIPTEN)
-    set_target_properties(gl3loadtests PROPERTIES SUFFIX ".html")
+    set_target_properties(es3loadtests PROPERTIES SUFFIX ".html")
 endif()
