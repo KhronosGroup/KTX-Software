0d670d6ed1808eb3c3a33bfa3096038458d140d9
MarkCallow <github@callow.im>
2020-06-11 14:48:53 -0700

    Adapt to Vulkan SDK becoming a .dmg.

--- ci_scripts/install_macos.sh
+++ ci_scripts/install_macos.sh
@@ -13,8 +13,8 @@ brew link sdl2
 
 # Current directory is .../build/{KhronosGroup,msc-}/KTX-Software. cd to 'build'.
 pushd ../..
-wget -O vulkansdk-macos-$VULKAN_SDK_VER.tar.gz https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VER/mac/vulkansdk-macos-$VULKAN_SDK_VER.tar.gz?Human=true
-tar -xzf vulkansdk-macos-$VULKAN_SDK_VER.tar.gz
+wget -O vulkansdk-macos-$VULKAN_SDK_VER.dmg https://sdk.lunarg.com/sdk/download/$VULKAN_SDK_VER/mac/vulkansdk-macos-$VULKAN_SDK_VER.dmg?Human=true
+hdiutil attach vulkansdk-macos-$VULKAN_SDK_VER.dmg
 wget -O Packages.dmg http://s.sudre.free.fr/Software/files/Packages.dmg
 hdiutil attach Packages.dmg
 sudo installer -pkg /Volumes/Packages*/packages/packages.pkg -target /
