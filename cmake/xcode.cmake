936bd86361d0c8978b7fa4497cbef49816b745df
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-06-16 18:35:24 +0200

    [CMake] Added option to build for release signing. Targets will be built with release RPATH right away. This prevents CMake from breaking already signed binaries by altering their RPATH. The downside is that executables don't load libraries correctly anymore if run in build directory.

--- cmake/xcode.cmake
+++ cmake/xcode.cmake
@@ -4,4 +4,11 @@ set_target_properties(${target} PROPERTIES
     XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "${XCODE_CODE_SIGN_IDENTITY}"
     XCODE_ATTRIBUTE_DEVELOPMENT_TEAM "${XCODE_DEVELOPMENT_TEAM}"
 )
+
+if(XCODE_CODE_SIGN_FOR_RELEASE)
+    set_target_properties(${target} PROPERTIES
+        BUILD_WITH_INSTALL_RPATH ON
+    )
+endif()
+
 endmacro (set_xcode_code_sign)
