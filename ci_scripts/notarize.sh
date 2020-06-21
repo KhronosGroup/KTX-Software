3c29a8cff2f207b0936d8acf8d8d9a240c198671
MarkCallow <github@callow.im>
2020-06-21 13:33:41 -0700

    Don't use keychain for any altool request.

--- ci_scripts/notarize.sh
+++ ci_scripts/notarize.sh
@@ -4,16 +4,23 @@ set -x
 
 # notarize.sh
 
+# Checks pkg for a valid signature. If valid, uploads the pkg for
+# notarization and monitors the notarization status
+
 # ©2020 - Mark Callow
 
 # Usage: notarize.sh <path/to/pkg> <appleid> <devteam> <password-label>
 #    path/to/pkg - path a signed .pkg file to be notarized.
 #    appleid - Apple developer login.
 #    devtem - 10 character development team identifier.
-#    password-label - the label of the altool app-specific password in your keychain.
-
-# Checks pkg for a valid signature. If valid, uploads the pkg for
-# notarization and monitors the notarization status
+#    password - app-specific password for altool to use when logging in to <applieid>.
+#               If the password is stored in the keychain then this argument should be
+#               of the form @keychain:<pw_label> where <pw_label> is the label
+#               of the password in the keychain.
+#
+# Retrieving this password from the keychain during a Travis-CI build is not working for
+# reasons that are not clear. altool is hanging. Clearly macOS security is asking
+# for permission for altool to access the keychain, even though this was set up.
 
 # Thanks to Armin Briegel for the script that inspired this one.
 
@@ -23,20 +30,20 @@ bundleid="com.khronos.ktx"
 # Code starts here
 
 if [[ $# != 4 ]]; then
-  echo "Usage: $0: <path/to/pkg> <appleid> <devteam> <password-label>"
+  echo "Usage: $0: <path/to/pkg> <appleid> <devteam> <password>"
   exit 1
 else
   pkg=$1
   appleid=$2
   devteam=$3
-  pw_label=$4
+  passwd=$4
 fi
 
 # functions
 requeststatus() { # $1: requestUUID
     req_status=$(xcrun altool --notarization-info "$requestUUID" \
                               --username "$appleid" \
-                              --password "@keychain:$pw_label" 2>&1 \
+                              --password "$passwd" 2>&1 \
                  | awk -F ': ' '/Status:/ { print $2; }' )
     echo "$req_status"
 }
@@ -48,7 +55,7 @@ notarizefile() { # $1: path to file to notarize, $2: bundle id.
     requestUUID=$(xcrun altool --notarize-app \
                                --primary-bundle-id "$2" \
                                --username "$appleid" \
-                               --password "$pw_label" \
+                               --password "$passwd" \
                                --asc-provider "$devteam" \
                                --file "$1" 2>&1 \
                   | awk '/RequestUUID/ { print $NF; }')
@@ -72,7 +79,7 @@ notarizefile() { # $1: path to file to notarize, $2: bundle id.
     # Print status information
     xcrun altool --notarization-info "$requestUUID" \
                  --username "$appleid" \
-                 --password "@keychain:$pw_label"
+                 --password "$passwd"
     echo
 
     if [[ $request_status != "success" ]]; then
@@ -93,9 +100,6 @@ if ! pkgutil --check-signature $pkg > /dev/null; then
   echo "Package does not have a valid signature."
   exit 2
 fi
-
-security unlock-keychain -p mysecretpassword build.keychain
-security set-keychain-settings -t 3600 -u build.keychain
 
 # Upload for notarization
 notarizefile "$pkg" "$bundleid"
