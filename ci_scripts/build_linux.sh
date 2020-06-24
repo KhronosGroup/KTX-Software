c0c325d328b946ad5f35c084b80af13b77802d93
Andreas Atteneder <andreas.atteneder@gmail.com>
2020-06-24 22:28:30 +0200

    fix: Try to update cmake (within docker container)

--- ci_scripts/build_linux.sh
+++ ci_scripts/build_linux.sh
@@ -34,6 +34,16 @@ popd
 
 # Emscripten/WebAssembly
 
+
+# Temporary solution: Update CMake. Can be dropped once the docker container updated the Ubuntu version
+echo "Update CMake for Web"
+docker exec -it emscripten sh -c "sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget"
+docker exec -it emscripten sh -c "wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null"
+docker exec -it emscripten sh -c "sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'"
+docker exec -it emscripten sh -c "sudo apt-get update"
+docker exec -it emscripten sh -c "sudo apt-get install cmake"
+
+
 echo "Configure/Build KTX-Software (Web Debug)"
 docker exec -it emscripten sh -c "emcmake cmake -Bbuild-web-debug . && cmake --build build-web-debug --config Debug"
 echo "Configure/Build KTX-Software (Web Release)"
@@ -43,11 +53,11 @@ docker exec -it emscripten sh -c "which cmake"
 docker exec -it emscripten sh -c "ls -l /usr/local/bin"
 docker exec -it emscripten sh -c "if [ -d /opt/cmake/bin ]; then ls -R /opt/cmake/bin; fi"
 
-pushd build-web-release
+# pushd build-web-release
 echo "Pack KTX-Software (Web Release)"
 # cpack is not in the Emscripter docker config.
 #docker exec -it -w $(pwd)/build-web-release emscripten sh -c "cpack --verbose -G ZIP"
-ls -R /home/travis/build
+# ls -R /home/travis/build
 # cpack --verbose -G ZIP
 docker exec -it emscripten sh -c "cmake --build build-web-release --config Release --target package"
-popd
+# popd
