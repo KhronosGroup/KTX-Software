d175ecd590173117504ffe9dd440a0c561db1099
MarkCallow <github@callow.im>
2020-07-23 16:33:53 -0700

    Save gen'ed file on error and call ktxinfo.

--- tests/toktx-tests.cmake
+++ tests/toktx-tests.cmake
@@ -53,12 +53,12 @@ PROPERTIES
 function( gencmpktx test_name reference source args env files )
     if(files)
         add_test( NAME toktx-cmp-${test_name}
-            COMMAND ${BASH_EXECUTABLE} -c "printf \"${files}\" > ${source} && $<TARGET_FILE:toktx> ${args} toktx.${reference} @${source} && diff ${reference} toktx.${reference}; rm toktx.${reference}; rm ${source}"
+            COMMAND ${BASH_EXECUTABLE} -c "printf \"${files}\" > ${source} && $<TARGET_FILE:toktx> ${args} toktx.${reference} @${source} && diff ${reference} toktx.${reference} && rm toktx.${reference}; rm ${source}"
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/testimages
         )
     else()
         add_test( NAME toktx-cmp-${test_name}
-            COMMAND ${BASH_EXECUTABLE} -c "$<TARGET_FILE:toktx> ${args} toktx.${reference} ${source} && diff ${reference} toktx.${reference} && rm toktx.${reference}"
+            COMMAND ${BASH_EXECUTABLE} -c "($<TARGET_FILE:toktx> ${args} toktx.${reference} ${source} && diff ${reference} toktx.${reference} && rm toktx.${reference}) || <TARGET_FILE:ktxinfo ${reference} toktx.${reference}"
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/testimages
         )
     endif()
