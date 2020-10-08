5f5b40be91acad43c868350ac2aa38d147726f0c
Moritz Willy Becher <becher@ux3d.io>
2020-10-08 15:41:15 +0200

    make ktx target includes and libs private

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -20,7 +20,7 @@ add_executable( unittests
 target_include_directories(
     unittests
 PRIVATE
-    $<TARGET_PROPERTY:ktx,INTERFACE_INCLUDE_DIRECTORIES>
+    $<TARGET_PROPERTY:ktx,INCLUDE_DIRECTORIES>
     ${PROJECT_SOURCE_DIR}/lib
 )
 
@@ -39,7 +39,7 @@ add_executable( texturetests
 target_include_directories(
     texturetests
 PRIVATE
-    $<TARGET_PROPERTY:ktx,INTERFACE_INCLUDE_DIRECTORIES>
+$<TARGET_PROPERTY:ktx,INCLUDE_DIRECTORIES>
     ${PROJECT_SOURCE_DIR}/other_include
     ${PROJECT_SOURCE_DIR}/lib
     unittests
