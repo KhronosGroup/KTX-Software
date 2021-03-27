e6ac10f496d84d72fe93e7f22a81d666e48bf3e1
Mark Callow <github@callow.im>
2021-03-27 12:52:24 +0900

    Fix typo.

--- ci_scripts/install_linux.sh
+++ ci_scripts/install_linux.sh
@@ -15,10 +15,8 @@ sudo apt-get -qq install libvulkan1 libvulkan-dev
 sudo apt-get -qq install libassimp5 libassimp-dev
 sudo apt-get -qq install rpm
 
-echo "wget LunarG key"
-wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
-echo "wget VulkanSDK version $VULKAN_SDK_VER".
-sudo wget -O etc/apt/sources.list.d/lunarg-vulkan-$VULKAN_SDK_VER-focal.list https://packages.lunarg.com/vulkan/$VULKAN_SDK_VER/lunarg-vulkan-$VULKAN_SDK_VER-focal.list
+wget -O - https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo apt-key add -
+sudo wget -O /etc/apt/sources.list.d/lunarg-vulkan-$VULKAN_SDK_VER-focal.list https://packages.lunarg.com/vulkan/$VULKAN_SDK_VER/lunarg-vulkan-$VULKAN_SDK_VER-focal.list
 echo "Install Vulkan SDK"
 sudo apt update
 sudo apt install vulkan-sdk
