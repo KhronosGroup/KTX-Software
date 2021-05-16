9ef77441a0ed0f8c004980f035a7c70947f8da02
Andreas Atteneder <andreas.atteneder@gmail.com>
2021-05-16 11:53:19 +0200

    fix: Android CI shaved off some build resources by only building one ABI in Debug configuration before building all ABIs in Release mode only.

--- ci_scripts/build_android.sh
+++ ci_scripts/build_android.sh
@@ -10,31 +10,10 @@ ANDROID_ABI=${ANDROID_ABI:-'arm64-v8a'}
 # You need to set the following environment variables first
 # ANDROID_NDK= <Path to Android NDK> 
 
-echo "Configure KTX-Software (Android $ANDROID_ABI Debug)"
-cmake . -G Ninja -B "build-android-$ANDROID_ABI-debug" \
--DANDROID_PLATFORM=android-24 \
--DANDROID_ABI="$ANDROID_ABI" \
--DANDROID_NDK="$ANDROID_NDK" \
--DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
--DCMAKE_BUILD_TYPE=Debug \
--DBASISU_SUPPORT_SSE=OFF
-
-pushd "build-android-$ANDROID_ABI-debug"
-
-echo "Build KTX-Software (Android $ANDROID_ABI Debug)"
-cmake --build . --config Debug -j
-# echo "Test KTX-Software (Android $ANDROID_ABI Debug)"
-# ctest -C Debug # --verbose
-echo "Install KTX-Software (Android $ANDROID_ABI Debug)"
-cmake --install . --config Debug --prefix ../install-android-debug/$ANDROID_ABI
-
-popd
-
-
 echo "Configure KTX-Software (Android $ANDROID_ABI Release)"
 cmake . -G Ninja -B "build-android-$ANDROID_ABI" \
 -DANDROID_PLATFORM=android-24 \
--DANDROID_ABI=$ANDROID_ABI \
+-DANDROID_ABI="$ANDROID_ABI" \
 -DANDROID_NDK="$ANDROID_NDK" \
 -DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
 -DCMAKE_BUILD_TYPE=Release \
