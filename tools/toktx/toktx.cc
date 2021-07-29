9c6702ad528b3fcef9d269b0eff2ad93d165e87f
unknown <tuketet@gmail.com>
2021-07-29 20:31:11 +0200

    support array textures

--- tools/toktx/toktx.cc
+++ tools/toktx/toktx.cc
@@ -717,9 +717,8 @@ toktxApp::main(int argc, _TCHAR *argv[])
     else
       createInfo.numFaces = 1;
 
-    // TO DO: handle array textures
     createInfo.numLayers = options.layers;
-    createInfo.isArray = KTX_FALSE;
+    createInfo.isArray = options.layers > 1;
 
     // TO DO: handle 3D textures.
 
@@ -1172,6 +1171,7 @@ toktxApp::main(int argc, _TCHAR *argv[])
                 level = 0;
                 levelWidth = createInfo.baseWidth;
                 levelHeight = createInfo.baseHeight;
+                levelDepth = createInfo.baseDepth;
                 if (faceSlice == (options.cubemap ? 6 : levelDepth)) {
                     faceSlice = 0;
                     layer++;
