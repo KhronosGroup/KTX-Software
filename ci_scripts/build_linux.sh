2d58c547fe36690f2e589dcf7364e613ab3b1fe3
Mark Callow <github@callow.im>
2021-08-20 14:53:02 +0900

    Print out toktx version for debugging.

--- ci_scripts/build_linux.sh
+++ ci_scripts/build_linux.sh
@@ -42,8 +42,12 @@ cpack -G RPM
 cpack -G TBZ2
 popd
 
-echo "***** libktx version.h *****"
-cat lib/version.h
+echo "***** toktx version.h *****"
+cat tools/toktx/version.h
+echo "****** toktx version ******"
+build/linux-release/tools/toktx/toktx --version
+echo "***************************"
+
 
 echo "Configure KTX-Software (Linux Debug without SSE support)"
 ${CMAKE_EXE} . -G Ninja -B$nosse_debug_build_dir -DCMAKE_BUILD_TYPE=Debug -DBASISU_SUPPORT_SSE=OFF
