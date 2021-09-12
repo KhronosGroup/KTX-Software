7004273e5d8877a65e7f66861227fa74ef1e2d57
Shukant Pal <sukantk2002@outlook.com>
2021-09-11 20:48:35 -0400

    Remove comments

--- interface/java_binding/src/test/java/org/khronos/ktx/test/KTXTestLibraryLoader.java
+++ interface/java_binding/src/test/java/org/khronos/ktx/test/KTXTestLibraryLoader.java
@@ -11,13 +11,8 @@ public class KTXTestLibraryLoader
 
     @Override
     public void beforeAll(final ExtensionContext context) throws Exception {
-        // lock the access so only one Thread has access to it
         if (!started) {
             started = true;
-            // Your "before all tests" startup logic goes here
-            // The following line registers a callback hook when the root test context is
-            // shut down
-            context.getRoot().getStore(GLOBAL).put("any unique name", this);
 
             System.loadLibrary("ktx");
             System.loadLibrary("ktx-jni");
@@ -26,6 +21,6 @@ public class KTXTestLibraryLoader
 
     @Override
     public void close() {
-        // Your "after all tests" logic goes here
+        // NOOP
     }
 }
