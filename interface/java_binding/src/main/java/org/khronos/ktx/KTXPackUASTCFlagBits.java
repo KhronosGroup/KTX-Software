5e67d995eab08bb03fecf478f3749138d86c1bdd
Shukant Pal <sukantk2002@outlook.com>
2021-09-11 20:47:21 -0400

    Remove prefix from constants

--- interface/java_binding/src/main/java/org/khronos/ktx/KTXPackUASTCFlagBits.java
+++ interface/java_binding/src/main/java/org/khronos/ktx/KTXPackUASTCFlagBits.java
@@ -1,16 +1,16 @@
 package org.khronos.ktx;
 
 public class KTXPackUASTCFlagBits {
-    public static final int KTX_PACK_UASTC_LEVEL_FASTEST = 0;
-    public static final int KTX_PACK_UASTC_LEVEL_FASTER = 1;
-    public static final int KTX_PACK_UASTC_LEVEL_DEFAULT = 2;
-    public static final int KTX_PACK_UASTC_LEVEL_SLOWER = 3;
-    public static final int KTX_PACK_UASTC_LEVEL_VERYSLOW = 4;
-    public static final int KTX_PACK_UASTC_MAX_LEVEL = KTX_PACK_UASTC_LEVEL_VERYSLOW;
-    public static final int KTX_PACK_UASTC_LEVEL_MASK = 0xF;
-    public static final int KTX_PACK_UASTC_FAVOR_UASTC_ERROR = 8;
-    public static final int KTX_PACK_UASTC_FAVOR_BC7_ERROR = 16;
-    public static final int KTX_PACK_UASTC_ETC1_FASTER_HINTS = 64;
-    public static final int KTX_PACK_UASTC_ETC1_FASTEST_HINTS = 128;
-    public static final int KTX_PACK_UASTC__ETC1_DISABLE_FLIP_AND_INDIVIDUAL = 256;
+    public static final int LEVEL_FASTEST = 0;
+    public static final int LEVEL_FASTER = 1;
+    public static final int LEVEL_DEFAULT = 2;
+    public static final int LEVEL_SLOWER = 3;
+    public static final int LEVEL_VERYSLOW = 4;
+    public static final int MAX_LEVEL = LEVEL_VERYSLOW;
+    public static final int LEVEL_MASK = 0xF;
+    public static final int FAVOR_UASTC_ERROR = 8;
+    public static final int FAVOR_BC7_ERROR = 16;
+    public static final int ETC1_FASTER_HINTS = 64;
+    public static final int ETC1_FASTEST_HINTS = 128;
+    public static final int ETC1_DISABLE_FLIP_AND_INDIVIDUAL = 256;
 }
