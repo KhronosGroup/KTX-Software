86df2e54b11f93273e19c692ab178e3e9e49f31a
Shukant Pal <sukantk2002@outlook.com>
2021-10-10 15:14:01 -0400

    Remove KtxSupercmpScheme.BASIS

--- interface/java_binding/src/main/java/org/khronos/ktx/KtxSupercmpScheme.java
+++ interface/java_binding/src/main/java/org/khronos/ktx/KtxSupercmpScheme.java
@@ -9,5 +9,4 @@ public class KtxSupercmpScheme {
     public static final int NONE = 0;
     public static final int BASIS_LZ = 1;
     public static final int ZSTD = 2;
-    public static final int BASIS = BASIS_LZ;
 }
