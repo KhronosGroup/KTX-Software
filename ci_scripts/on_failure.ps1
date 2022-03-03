db1253ff468f213fb5592d1ff4f04a09853ab0c5
Mark Callow <github@callow.im>
2022-03-03 22:28:16 +0900

    Fix syntax errors.

--- ci_scripts/on_failure.ps1
+++ ci_scripts/on_failure.ps1
@@ -8,7 +8,7 @@ if (! $env:Phase) {
 echo "Now uploading the failed tests"
 ls -Name $env:APPVEYOR_BUILD_FOLDER/tests/testimages/toktx*
 tar -cvf failed-tests.tar $env:APPVEYOR_BUILD_FOLDER/tests/testimages/toktx*
-curl --upload-file failed-tests.tar https://transfer.sh/toktx-failed-tests.tar)
+curl --upload-file failed-tests.tar https://transfer.sh/toktx-failed-tests.tar
 echo "Now uploading the test log"
-curl --upload-file $env:APPVEYOR_BUILD_FOLDER/$env:BUILD_DIR/Testing/Temporary/LastTest.log https://transfer.sh/ktx-last-test.log)
+curl --upload-file $env:APPVEYOR_BUILD_FOLDER/$env:BUILD_DIR/Testing/Temporary/LastTest.log https://transfer.sh/ktx-last-test.log
 
