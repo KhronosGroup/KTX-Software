c769a2b4375b9a50a9bf32d0783dfdf20281d5a2
Mark Callow <github@callow.im>
2022-03-07 13:17:45 +0900

    On Appveyor only build tools and packge when necessary.
    On travis try ": " instead of "=" for single env var assignments.

--- ci_scripts/build_win.ps1
+++ ci_scripts/build_win.ps1
@@ -9,10 +9,13 @@ $CONFIGURATION `
   = $(if ($env:CONFIGURATION) { $env:CONFIGURATION } else { "Release" })
 $CMAKE_GEN `
   = $(if ($env:CMAKE_GEN) { $env:CMAKE_GEN } else { "Visual Studio 16 2019" })
-$PLATFORM = $(if ($env:PLATFORM) { $env:PLATFORM } else { "x64" })
 $FEATURE_DOC = $(if ($env:FEATURE_DOC) { $env:FEATURE_DOC } else { "OFF" })
+$FEATURE_JNI = $(if ($env:FEATURE_JNI) { $env:FEATURE_JNI } else { "OFF" })
 $FEATURE_LOADTESTS `
   = $(if ($env:FEATURE_LOADTESTS) { $env:FEATURE_LOADTESTS } else { "OFF" })
+$FEATURE_TOOLS = $(if ($env:FEATURE_TOOLS) { $env:FEATURE_TOOLS } else { "ON" })
+$PLATFORM = $(if ($env:PLATFORM) { $env:PLATFORM } else { "x64" })
+$PACKAGE = $(if ($env:PACKAGE) { $env:PACKAGE } else { "NO" })
 $SUPPORT_SSE = $(if ($env:SUPPORT_SSE) { $env:SUPPORT_SSE } else { "OFF" })
 $SUPPORT_OPENCL `
    = $(if ($env:SUPPORT_OPENCL) { $env:SUPPORT_OPENCL } else { "OFF" })
@@ -28,8 +31,10 @@ if ($FEATURE_LOADTESTS -eq "ON")  { $need_gles_emulator=1 }
 echo "Build via $CMAKE_GEN dir: $build_dir Arch: $PLATFORM Config: $CONFIGURATION FEATURE_LOADTESTS: $FEATURE_LOADTESTS FEATURE_DOC: $FEATURE_DOC SUPPORT_SSE: $SUPPORT_SSE SUPPORT_OPENCL: $SUPPORT_OPENCL"
 
 cmake . -G "$CMAKE_GEN" -A $PLATFORM -B $BUILD_DIR `
-  -D KTX_FEATURE_LOADTEST_APPS=$FEATURE_LOADTESTS `
   -D KTX_FEATURE_DOC=$FEATURE_DOC `
+  -D KTX_FEATURE_JNI=$FEATURE_JNI `
+  -D KTX_FEATURE_LOADTEST_APPS=$FEATURE_LOADTESTS `
+  -D KTX_FEATURE_TOOLS=$FEATURE_TOOLS `
   -D BASISU_SUPPORT_SSE=$SUPPORT_SSE `
   -D BASISU_SUPPORT_OPENCL=$SUPPORT_OPENCL `
   $(if ($need_gles_emulator) {"-D"}) $(if ($need_gles_emulator) {"OPENGL_ES_EMULATOR=$OPENGL_ES_EMULATOR"})
@@ -39,7 +44,9 @@ try {
   #git status
   cmake --build . --config $CONFIGURATION
   #git status
-  cmake --build . --config $CONFIGURATION --target PACKAGE
+  if ($PACKAGE -eq "YES") {
+    cmake --build . --config $CONFIGURATION --target PACKAGE
+  }
   echo "Done building."
 } finally {
   popd
