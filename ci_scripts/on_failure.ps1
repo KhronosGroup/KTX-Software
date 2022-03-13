fbd62645a90b73dbf999368949d423b1d4b3ea13
Mark Callow <github@callow.im>
2022-03-13 13:35:06 +0900

    Don't use `exit`.

--- ci_scripts/on_failure.ps1
+++ ci_scripts/on_failure.ps1
@@ -2,15 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 echo "Phase = $env:Phase"
-if (! $env:Phase) {
-  echo "Exiting on_failure script."
-  exit
+if ($env:Phase) {
+    echo "Now uploading the failed tests"
+    ls -Name $env:APPVEYOR_BUILD_FOLDER/tests/testimages/toktx*
+    tar -cvf failed-tests.tar $env:APPVEYOR_BUILD_FOLDER/tests/testimages/toktx*
+    curl --upload-file failed-tests.tar https://transfer.sh/toktx-failed-tests.tar
+    echo "Now uploading the test log"
+    curl --upload-file $env:APPVEYOR_BUILD_FOLDER/$env:BUILD_DIR/Testing/Temporary/LastTest.log https://transfer.sh/ktx-last-test.log
 }
-
-echo "Now uploading the failed tests"
-ls -Name $env:APPVEYOR_BUILD_FOLDER/tests/testimages/toktx*
-tar -cvf failed-tests.tar $env:APPVEYOR_BUILD_FOLDER/tests/testimages/toktx*
-curl --upload-file failed-tests.tar https://transfer.sh/toktx-failed-tests.tar
-echo "Now uploading the test log"
-curl --upload-file $env:APPVEYOR_BUILD_FOLDER/$env:BUILD_DIR/Testing/Temporary/LastTest.log https://transfer.sh/ktx-last-test.log
-
