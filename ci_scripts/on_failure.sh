64ce059b5de4896d05d8f31c95c2f930ef6057d7
Mark Callow <github@callow.im>
2022-04-04 17:05:27 +0900

    Only try to upload failed tests on successful tar.

--- ci_scripts/on_failure.sh
+++ ci_scripts/on_failure.sh
@@ -2,13 +2,15 @@
 # Copyright 2022 The Khronos Group Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-echo "Now uploading the failed tests"
-tar -cvf failed-tests.tar $TRAVIS_BUILD_DIR/tests/testimages/toktx*
-# curl/transfer.sh prints the retrieval URL on completion. As the output
-# is not terminated with a new-line it does not show up in Travis-CI's
-# cooked output, only the raw. Workaround by saving to variable.
-rurl=$(curl --upload-file failed-tests.tar https://transfer.sh/toktx-failed-tests.tar)
-echo $rurl
+echo "Now checking for and uploading any failed tests"
+if tar -cvf failed-tests.tar $TRAVIS_BUILD_DIR/tests/testimages/toktx*
+then
+  # curl/transfer.sh prints the retrieval URL on completion. As the output
+  # is not terminated with a new-line it does not show up in Travis-CI's
+  # cooked output, only the raw. Workaround by saving to variable.
+  rurl=$(curl --upload-file failed-tests.tar https://transfer.sh/toktx-failed-tests.tar)
+  echo $rurl
+fi
 echo "Now uploading the test log"
 rurl=$(curl --upload-file $TRAVIS_BUILD_DIR/$BUILD_DIR/Testing/Temporary/LastTest.log https://transfer.sh/ktx-failed-tests.log)
 echo $rurl
