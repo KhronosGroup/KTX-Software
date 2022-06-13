429d5f1a343615782c6a11ac0ca5bf7961b6f6d1
Honeybunch <amt3824@g.rit.edu>
2022-06-13 16:21:48 -0700

    Fixing up some small issues

--- lib/etcdec.cxx
+++ lib/etcdec.cxx
@@ -178,7 +178,7 @@ submitted to the exclusive jurisdiction of the Swedish Courts.
 #if __clang__
   #pragma clang diagnostic push
   #pragma clang diagnostic ignored "-Wunused-parameter"
-  #if __clang_major__ > 13 || (__clang_major__ == 13 && __clang_minor__ >= 1)
+  #if __has_warning("-Wunused-but-set-variable")
     #pragma clang diagnostic ignored "-Wunused-but-set-variable"
   #endif
 #elif __GNUC__
