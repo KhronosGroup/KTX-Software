86c18e8ee2f13deef4552633f551beb6610ed257
Mark Callow <github@callow.im>
2022-06-17 18:42:44 +0900

    Use on_failure in GitHub Action.

--- ci_scripts/on_failure.ps1
+++ ci_scripts/on_failure.ps1
@@ -1,12 +1,26 @@
 # Copyright 2022 The Khronos Group Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+if ($args[0]) { $repo_root = $args[0] } else { $repo_root = "." }
+if ($args[1]) { $build_dir = $args[1] } else { $build_dir = "build" }
+
+# In Github Actions, this is called only when there is a failed test step.
+# In Appveyor we use the Phase environment variable.
 echo "Phase = $env:Phase"
-if ($env:Phase) {
-    echo "Now uploading the failed tests"
-    ls -Name $env:APPVEYOR_BUILD_FOLDER/tests/testimages/toktx*
-    tar -cvf failed-tests.tar $env:APPVEYOR_BUILD_FOLDER/tests/testimages/toktx*
-    curl --upload-file failed-tests.tar https://transfer.sh/toktx-failed-tests.tar
-    echo "Now uploading the test log"
-    curl --upload-file $env:APPVEYOR_BUILD_FOLDER/$env:BUILD_DIR/Testing/Temporary/LastTest.log https://transfer.sh/ktx-last-test.log
+if ($env:GITHUB_ACTIONS -or $env:Phase) {
+  pushd $repo_root
+  echo "Now uploading the failed tests"
+  ls -Name  tests/testimages/ktxsc* tests/testimages/toktx*
+  if (tar -cvf failed-images.tar tests/testimages/ktxsc* tests/testimages/toktx*) {
+    # N.B. In PS, "curl" is an alias for Invoke-WebRequest. Uploading a
+    # file via that looks like a p.i.t.a so use the real curl command.
+    curl.exe --upload-file failed-images.tar https://transfer.sh/ktx-failed-images.tar
+  }
+  # Even if there are no failed images and tar exits with false it creates
+  # the output file.
+  rm failed-images.tar
+
+  echo "`r`nNow uploading the test log"
+  curl.exe --upload-file $build_dir/Testing/Temporary/LastTest.log https://transfer.sh/ktx-last-test.log
+  popd
 }
