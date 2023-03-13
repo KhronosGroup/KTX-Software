2e6fe34f41fd0cd903c7459524688e5c9f240de7
Mark Callow <github@callow.im>
2023-03-13 14:33:36 +0900

    Change cmake config arg handling to new style.

--- ci_scripts/build_android_debug.sh
+++ ci_scripts/build_android_debug.sh
@@ -8,20 +8,37 @@ set -e
 ANDROID_ABI=${ANDROID_ABI:-'arm64-v8a'}
 ASTC_ISA=${ASTC_ISA:-'ISA_NONE=ON'}
 
+BUILD_DIR="build-android-$ANDROID_ABI-debug"
+
 # You need to set the following environment variables first
 # ANDROID_NDK= <Path to Android NDK>
 
-echo "Configure KTX-Software (Android $ANDROID_ABI Debug)"
-cmake . -G Ninja -B "build-android-$ANDROID_ABI-debug" \
--DANDROID_PLATFORM=android-24 \
--DANDROID_ABI="$ANDROID_ABI" \
--DANDROID_NDK="$ANDROID_NDK" \
--DCMAKE_TOOLCHAIN_FILE="$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
--DCMAKE_BUILD_TYPE=Debug \
--DBASISU_SUPPORT_SSE=OFF \
--D${ASTC_ISA}
+cmake_args=("-G" "Ninja" \
+  "-B" "$BUILD_DIR" \
+  "-D" "ANDROID_PLATFORM=android-24" \
+  "-D" "ANDROID_ABI=$ANDROID_ABI" \
+  "-D" "ANDROID_NDK=$ANDROID_NDK" \
+  "-D" "CMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
+  "-D" "CMAKE_BUILD_TYPE=Debug" \
+  "-D" "BASISU_SUPPORT_SSE=OFF" \
+  "-D" "${ASTC_ISA}"
+)
 
-pushd "build-android-$ANDROID_ABI-debug"
+config_display="Configure KTX-Software (Android $ANDROID_ABI Debug): "
+for arg in "${cmake_args[@]}"; do
+  echo $arg
+  case $arg in
+    "-G") config_display+="Generator=" ;;
+    "-B") config_display+="Build Dir=" ;;
+    "-D") ;;
+    *) config_display+="$arg, " ;;
+  esac
+done
+
+echo ${config_display%??}
+cmake . "${cmake_args[@]}"
+
+pushd "$BUILD_DIR"
 
 echo "Build KTX-Software (Android $ANDROID_ABI Debug)"
 cmake --build . --config Debug -j
