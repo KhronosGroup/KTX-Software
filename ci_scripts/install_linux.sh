b6c4db8a43e3f3f70627a433860dd40b09ce66a2
Mark Callow <github@callow.im>
2023-04-30 15:23:05 +0900

    Fix lunarg key installation problem.

--- ci_scripts/install_linux.sh
+++ ci_scripts/install_linux.sh
@@ -32,7 +32,8 @@ if [ "$FEATURE_LOADTESTS" = "ON" ]; then
   os_codename=${os_codename#VERSION_CODENAME=}
 
   echo "Download Vulkan SDK"
-  sudo wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc > /etc/apt/trusted.gpg.d/lunarg.asc
+  # tee is used (and elevated with sudo) so we can write to the destination.
+  wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc > /dev/null
   sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-$VULKAN_SDK_VER-$os_codename.list https://packages.lunarg.com/vulkan/$VULKAN_SDK_VER/lunarg-vulkan-$VULKAN_SDK_VER-$os_codename.list
 
   echo "Install Vulkan SDK"
