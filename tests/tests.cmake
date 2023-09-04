76bf1770fecfffaa977dbb78b7b85788da2d056d
Mark Callow <github@callow.im>
2023-09-04 18:07:30 +0900

    Link unittests to {fmt} lib and specify C++17.

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -31,8 +31,8 @@ add_subdirectory(transcodetests)
 add_subdirectory(streamtests)
 
 add_executable( unittests
-    unittests/unittests.cc
     unittests/image_unittests.cc
+    unittests/unittests.cc
     unittests/wthelper.h
     tests.cmake
 )
@@ -60,7 +60,15 @@ target_link_libraries(
     unittests
     gtest
     ktx
+    fmt::fmt
     ${CMAKE_THREAD_LIBS_INIT}
+)
+
+set_target_properties(
+    unittests
+    PROPERTIES
+        CXX_STANDARD 17
+        CXX_STANDARD_REQUIRED YES
 )
 
 add_executable( texturetests
