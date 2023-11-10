fbcc15ef42107d118a5cbfe2938a3076edacdb3c
shukantpal <foss@shukantpal.com>
2023-11-10 08:17:48 -0800

    Set path to single folder

--- interface/python_binding/buildscript.py
+++ interface/python_binding/buildscript.py
@@ -23,7 +23,7 @@ elif os.name == 'osx':
     if LIBKTX_LIB_DIR is None:
         LIBKTX_LIB_DIR = '/usr/local/lib'
 
-print(f"TEST LIBDIR IS {os.getenv('LIBKTX_LIB_DIR')}")
+print(f"TEST PATH IS {os.getenv('PATH')}")
 
 ffibuilder = FFI()
 
