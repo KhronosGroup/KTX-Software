97531331db9720eefc8e523c6e180483fb3ca285
ShukantP <security@shukantpal.com>
2023-11-12 16:59:36 -0800

    fix deleted pyktx-dep ref

--- out/build/x64-Debug/tests/streamtests/streamtests[1]_include.cmake
+++ out/build/x64-Debug/tests/streamtests/streamtests[1]_include.cmake
@@ -1,1 +1,23 @@
-include("C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/out/build/x64-Debug/tests/streamtests/streamtests[1]_include.cmake")
+if(EXISTS "C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/out/build/x64-Debug/Debug/streamtests.exe")
+  if("C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/out/build/x64-Debug/Debug/streamtests.exe" IS_NEWER_THAN "C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/out/build/x64-Debug/tests/streamtests/streamtests[1]_tests.cmake")
+    include("C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/share/cmake-3.20/Modules/GoogleTestAddTests.cmake")
+    gtest_discover_tests_impl(
+      TEST_EXECUTABLE [==[C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/out/build/x64-Debug/Debug/streamtests.exe]==]
+      TEST_EXECUTOR [==[]==]
+      TEST_WORKING_DIR [==[C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/out/build/x64-Debug/tests/streamtests]==]
+      TEST_EXTRA_ARGS [==[C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/tests/testimages/]==]
+      TEST_PROPERTIES [==[]==]
+      TEST_PREFIX [==[streamtest]==]
+      TEST_SUFFIX [==[]==]
+      NO_PRETTY_TYPES [==[FALSE]==]
+      NO_PRETTY_VALUES [==[FALSE]==]
+      TEST_LIST [==[streamtests_TESTS]==]
+      CTEST_FILE [==[C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/out/build/x64-Debug/tests/streamtests/streamtests[1]_tests.cmake]==]
+      TEST_DISCOVERY_TIMEOUT [==[20]==]
+      TEST_XML_OUTPUT_DIR [==[]==]
+    )
+  endif()
+  include("C:/Users/Shuka/source/repos/KhronosGroup/KTX-Software/out/build/x64-Debug/tests/streamtests/streamtests[1]_tests.cmake")
+else()
+  add_test(streamtests_NOT_BUILT streamtests_NOT_BUILT)
+endif()
