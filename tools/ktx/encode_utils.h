f557966f2a8c0a14162a2c023d5a23a085d70346
Wasim Abbas <wasim.abbas@arm.com>
2023-12-09 18:36:21 +0000

    Abstract encode common options

--- tools/ktx/encode_utils.h
+++ tools/ktx/encode_utils.h
@@ -144,44 +144,8 @@ enum class EncodeCodec {
             deterministic).</dd>
     </dl>
 
-    <dt>
-        common:
-    </dt>
-    <dd>
-        Common options.
-    </dd>
+    @snippet{doc} ktx/encode_utils_common.h command options_codec_common
 
-    <dl>
-        <dt>\--normal-mode</dt>
-        <dd>Only valid for linear textures with two or more components.
-            If the input texture has three or four linear components it is
-            assumed to be a three component linear normal map storing unit
-            length normals as (R=X, G=Y, B=Z). A fourth component will be
-            ignored. The map will be converted to a two component X+Y
-            normal map stored as (RGB=X, A=Y) prior to encoding. If unsure
-            that your normals are unit length, use @b --normalize. If the
-            input has 2 linear components it is assumed to be an X+Y map
-            of unit normals.
-
-            The Z component can be recovered programmatically in shader
-            code by using the equations:
-            <pre>
-    nml.xy = texture(...).ga;              // Load in [0,1]
-    nml.xy = nml.xy * 2.0 - 1.0;           // Unpack to [-1,1]
-    nml.z = sqrt(1 - dot(nml.xy, nml.xy)); // Compute Z
-            </pre>
-            For ETC1S / BasisLZ encoding, @b '--encode basis-lz', RDO is disabled
-            (no selector RDO, no endpoint RDO) to provide better quality.</dd>
-        <dt>\--threads &lt;count&gt;</dt>
-        <dd>Explicitly set the number of threads to use during
-            compression. By default, ETC1S / BasisLZ will use the number of
-            threads reported by thread::hardware_concurrency or 1 if value
-            returned is 0.</dd>
-        <dt>\--no-sse</dt>
-        <dd>Forbid use of the SSE instruction set. Ignored if CPU does
-            not support SSE. SSE can only be disabled on the basis-lz and
-            uastc compressors.</dd>
-    </dl>
 </dl>
 //! [command options_codec]
 */
@@ -318,24 +282,6 @@ struct OptionsCodec {
                 cxxopts::value<float>(), "<deviation>")
             (kUastcRdoF, "Do not favor simpler UASTC modes in RDO mode.")
             (kUastcRdoM, "Disable RDO multithreading (slightly higher compression, deterministic).");
-        opts.add_options("Encode common")
-            (kNormalMode, "Optimizes for encoding textures with normal data. If the input texture has "
-                "three or four linear components it is assumed to be a three component linear normal "
-                "map storing unit length normals as (R=X, G=Y, B=Z). A fourth component will be ignored. "
-                "The map will be converted to a two component X+Y normal map stored as (RGB=X, A=Y) "
-                "prior to encoding. If unsure that your normals are unit length, use --normalize. "
-                "If the input has 2 linear components it is assumed to be an X+Y map of unit normals.\n"
-                "The Z component can be recovered programmatically in shader code by using the equations:\n"
-                "    nml.xy = texture(...).ga;              // Load in [0,1]\n"
-                "    nml.xy = nml.xy * 2.0 - 1.0;           // Unpack to [-1,1]\n"
-                "    nml.z = sqrt(1 - dot(nml.xy, nml.xy)); // Compute Z\n"
-                "ETC1S / BasisLZ encoding, RDO is disabled (no selector RDO, no endpoint RDO) to provide better quality.")
-            (kThreads, "Sets the number of threads to use during encoding. By default, encoding "
-                "will use the number of threads reported by thread::hardware_concurrency or 1 if "
-                "value returned is 0.", cxxopts::value<uint32_t>(), "<count>")
-            (kNoSse, "Forbid use of the SSE instruction set. Ignored if CPU does "
-               "not support SSE. SSE can only be disabled on the basis-lz and "
-               "uastc compressors.");
     }
 
     EncodeCodec validateEncodeCodec(const cxxopts::OptionValue& codecOpt) const {
@@ -534,25 +480,6 @@ struct OptionsCodec {
             validateUASTCRDOArg(report, kUastcRdoM);
             captureCodecOption(kUastcRdoM);
             basisOpts.uastcRDONoMultithreading = 1;
-        }
-
-        if (args[kNormalMode].count()) {
-            validateCommonEncodeArg(report, kNormalMode);
-            captureCodecOption(kNormalMode);
-            basisOpts.normalMap = true;
-        }
-
-        if (args[kThreads].count()) {
-            validateCommonEncodeArg(report, kThreads);
-            basisOpts.threadCount = captureCodecOption<uint32_t>(args, kThreads);
-        } else {
-            basisOpts.threadCount = std::thread::hardware_concurrency();
-        }
-
-        if (args[kNoSse].count()) {
-            validateCommonEncodeArg(report, kNoSse);
-            captureCodecOption(kNoSse);
-            basisOpts.noSSE = true;
         }
     }
 };
