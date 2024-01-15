556372fc0ee45690df54e70b3cdefb48210102be
Mark Callow <github@callow.im>
2024-01-15 11:16:40 +0900

    Add asset download script. Use it in publish_pyktx.

---
+++ ci_scripts/download_release_assets.sh
@@ -0,0 +1,58 @@
+#! /usr/bin/env bash
+
+function usage() {
+  echo "Usage: $0 [--help] [--pre] [--filter <pattern>]"
+  echo ""
+  echo "Download the assets of the latest KTX-Software release from the"
+  echo "GitHub repo."
+  echo ""
+  echo "Options:"
+  echo "  --filter, -f <pattern>"
+  echo "             Only download assets whose name matches <pattern> where"
+  echo "             pattern is a regular expression."
+  echo "  --help, -h Print this usage message."
+  echo "  --output-dir <directory>"
+  echo "             Save the downloaded files in <directory>. Defaults to"
+  echo "             the current directory."
+  echo "  --pre-release, -p"
+  echo "             Retrieve assets from latest pre-release."
+  exit $1
+}
+
+# list_releases.sh should be colocated with this script
+# and the directory may not be in $PATH.
+get_release=$(dirname $0)/list_releases.sh
+
+target="--latest"
+output_dir="."
+
+while [ $# -ne 0 ]; do
+  case $1 in
+    --filter | -f)
+      if [ $# -lt 1 ]; then
+        usage 1
+      else
+        pattern=$2
+        shift 2
+      fi
+      ;;
+    --help | -h)
+      usage 0;;
+    --output-dir | -o)
+      if [ $# -lt 1 ]; then
+        usage 1
+      else
+        output_dir=$2
+        shift 2
+      fi
+      ;;
+    --pre-release | -p)
+      target="--latest-pre"
+      shift;;
+    *) usage 1;;
+  esac
+done
+
+# Retrieve target release metadata, extract and filter assets and extract
+# their browser_download_urls using jq (jquery).
+jq ".assets | map(select(.name | test(\"$pattern\"))) | map(.browser_download_url)" <<< $($get_release $target) | jq -c '.[]' | xargs -L 1 curl -O -J -L --create-dirs --output-dir $output_dir
---
+++ ci_scripts/list_releases.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+
+function usage() {
+  echo "Usage: $0 [--help | --latest | --latest-pre]"
+  echo ""
+  echo "With no option retrieves the list of releases from the KTX-Software"
+  echo "GitHub repo."
+  echo ""
+  echo "Options:"
+  echo "  --help, -h       Print this usage message."
+  echo "  --latest, -l     Retrieve information about latest release."
+  echo "  --latest-pre, -p Retrieve information about latest pre-release."
+  exit $1
+}
+
+ktx_repo_url=https://api.github.com/repos/KhronosGroup/KTX-Software
+
+function get_release_list() {
+  curl --silent -L $ktx_repo_url/releases
+}
+
+if [ $# -eq 0 ]; then
+  get_release_list
+  exit 0
+elif [ $# -eq 1 ]; then
+  case $1 in
+    --help | -h)
+      usage 0 ;;
+    --latest-pre | -p)
+      release_url=$(jq -r 'map(select(.prerelease)) | first | .url' <<< $(get_release_list))
+      ;;
+    --latest | -l)
+      release_url=$ktx_repo_url/releases/latest
+      ;;
+    *) usage 1 ;;
+  esac
+else
+  usage 1
+fi
+
+curl \
+  --silent -L\
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  $release_url
