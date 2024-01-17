394eb46e3c31b7f94e0040028ac90758d60c79bf
Mark Callow <github@callow.im>
2024-01-17 16:43:01 +0900

    Add script for checking mkvk.

---
+++ ci_scripts/check_mkvk.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/env bash
+# Copyright 2015-2020 The Khronos Group Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+# Check generation of VkFormat related files.
+
+BUILD_DIR=${BUILD_DIR:-build/checkmkvk}
+
+cmake . -B $BUILD_DIR -D KTX_FEATURE_TESTS=OFF -D KTX_FEATURE_TOOLS=OFF -D KTX_GENERATE_VK_FILES=ON
+echo "cmake config status is $?"
+cmake --build $BUILD_DIR --target mkvk
+echo "cmake build status is $?"
+# Verify no files were modified. Exits with 1, if so.
+git status
+if git diff-index --quiet HEAD; then
+    git status
+    git diff-index HEAD
+    git diff
+    exit 1
+fi
