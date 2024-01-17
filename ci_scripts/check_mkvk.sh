877c6ff3df4ffe29d3b0ddf30c5df0b4c9942dcf
Mark Callow <github@callow.im>
2024-01-17 17:28:20 +0900

    Do --clean-first to force generating all files.

--- ci_scripts/check_mkvk.sh
+++ ci_scripts/check_mkvk.sh
@@ -7,9 +7,8 @@
 BUILD_DIR=${BUILD_DIR:-build/checkmkvk}
 
 cmake . -B $BUILD_DIR -D KTX_FEATURE_TESTS=OFF -D KTX_FEATURE_TOOLS=OFF -D KTX_GENERATE_VK_FILES=ON
-echo "cmake config status is $?"
-cmake --build $BUILD_DIR --target mkvk
-echo "cmake build status is $?"
+# Clean first is to ensure all files are generated so everything is tested.
+cmake --build $BUILD_DIR --target mkvk --clean-first
 # Verify no files were modified. Exit with 1, if so.
 if ! git diff --quiet HEAD; then
     git status
