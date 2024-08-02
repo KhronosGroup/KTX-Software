9744219205ba623d040c2986b2405a9d7fadb183
Mark Callow <github@callow.im>
2024-08-02 12:30:41 +0900

    Fix syntax error.

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -113,7 +113,7 @@ IFS=, ; for config in $CONFIGURATION
 do
   IFS=$oldifs # Because of ; IFS set above will still be present.
   # Build and test
-  if [ "$config" = "Debug" ]; continue; fi
+  if [ "$config" = "Debug" ]; then; continue; fi
   echo "Build KTX-Software (macOS $ARCHS $config)"
   if [ -n "$MACOS_CERTIFICATES_P12" -a "$config" = "Release" ]; then
     cmake --build . --config $config | handle_compiler_output
