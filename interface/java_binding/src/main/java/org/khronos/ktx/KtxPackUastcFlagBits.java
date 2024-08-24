ada93e2259e8391b93136378bb953772850ec915
Marco Hutter <javagl@javagl.de>
2024-08-24 15:13:31 +0200

    Update KtxPackUastcFlagBits names

--- interface/java_binding/src/main/java/org/khronos/ktx/KtxPackUastcFlagBits.java
+++ interface/java_binding/src/main/java/org/khronos/ktx/KtxPackUastcFlagBits.java
@@ -12,70 +12,69 @@ package org.khronos.ktx;
  * These bits can be passed to {@link KtxBasisParams#setUastcFlags(int)}.<br>
  * <br>
  * The most important value is the level given in the least-significant
- * 4 bits which selects a speed vs quality tradeoff. These are the
- * <code>KTX_PACK_UASTC_LEVEL_...</code> constants.
+ * 4 bits which selects a speed vs quality tradeoff.
  */
 public class KtxPackUastcFlagBits {
 
 	/**
 	 * Fastest compression. 43.45dB.
 	 */
-    public static final int KTX_PACK_UASTC_LEVEL_FASTEST = 0;
+    public static final int FASTEST = 0;
 
     /**
      * Faster compression. 46.49dB.
      */
-    public static final int KTX_PACK_UASTC_LEVEL_FASTER = 1;
+    public static final int FASTER = 1;
 
     /**
      * Default compression. 47.47dB.
      */
-    public static final int KTX_PACK_UASTC_LEVEL_DEFAULT = 2;
+    public static final int DEFAULT = 2;
 
     /**
      * Slower compression. 48.01dB.
      */
-    public static final int KTX_PACK_UASTC_LEVEL_SLOWER = 3;
+    public static final int SLOWER = 3;
 
     /**
      * Very slow compression. 48.24dB.
      */
-    public static final int KTX_PACK_UASTC_LEVEL_VERYSLOW = 4;
+    public static final int VERYSLOW = 4;
 
     /**
      * The maximum quality level
      */
-    public static final int KTX_PACK_UASTC_MAX_LEVEL = KTX_PACK_UASTC_LEVEL_VERYSLOW;
+    public static final int MAX_LEVEL = VERYSLOW;
 
     /**
      * Mask to extract the level from the other bits
      */
-    public static final int KTX_PACK_UASTC_LEVEL_MASK = 0xF;
+    public static final int MASK = 0xF;
 
     /**
      * Optimize for lowest UASTC error
      */
-    public static final int KTX_PACK_UASTC_FAVOR_UASTC_ERROR = 8;
+    public static final int FAVOR_UASTC_ERROR = 8;
 
     /**
      * Optimize for lowest BC7 error
      */
-    public static final int KTX_PACK_UASTC_FAVOR_BC7_ERROR = 16;
+    public static final int FAVOR_BC7_ERROR = 16;
 
     /**
      * Optimize for faster transcoding to ETC1
      */
-    public static final int KTX_PACK_UASTC_ETC1_FASTER_HINTS = 64;
+    public static final int ETC1_FASTER_HINTS = 64;
 
     /**
      * Optimize for fastest transcoding to ETC1
      */
-    public static final int KTX_PACK_UASTC_ETC1_FASTEST_HINTS = 128;
+    public static final int ETC1_FASTEST_HINTS = 128;
 
     /**
      * Not documented in BasisU code
      */
-    public static final int KTX_PACK_UASTC_ETC1_DISABLE_FLIP_AND_INDIVIDUAL = 256;
+    public static final int ETC1_DISABLE_FLIP_AND_INDIVIDUAL = 256;
 
 
 	/**
@@ -86,45 +85,45 @@ public class KtxPackUastcFlagBits {
 	 */
 	public static String stringFor(int n) {
 		StringBuilder sb = new StringBuilder();
-		int level = n & KTX_PACK_UASTC_LEVEL_MASK;
+		int level = n & MASK;
 
 		switch (level) {
-		case KTX_PACK_UASTC_LEVEL_FASTEST: sb.append("KTX_PACK_UASTC_LEVEL_FASTEST"); break;
-		case KTX_PACK_UASTC_LEVEL_FASTER : sb.append("KTX_PACK_UASTC_LEVEL_FASTER"); break;
-		case KTX_PACK_UASTC_LEVEL_DEFAULT: sb.append("KTX_PACK_UASTC_LEVEL_DEFAULT"); break;
-		case KTX_PACK_UASTC_LEVEL_SLOWER: sb.append("KTX_PACK_UASTC_LEVEL_SLOWER"); break;
-		case KTX_PACK_UASTC_LEVEL_VERYSLOW: sb.append("KTX_PACK_UASTC_LEVEL_VERYSLOW"); break;
+		case FASTEST: sb.append("FASTEST"); break;
+		case FASTER : sb.append("FASTER"); break;
+		case DEFAULT: sb.append("DEFAULT"); break;
+		case SLOWER: sb.append("SLOWER"); break;
+		case VERYSLOW: sb.append("VERYSLOW"); break;
 		}
 
-		if ((n & KTX_PACK_UASTC_FAVOR_UASTC_ERROR) != 0) {
+		if ((n & FAVOR_UASTC_ERROR) != 0) {
 			if (sb.length() != 0) {
 				sb.append("|");
 			}
-			sb.append("KTX_PACK_UASTC_FAVOR_UASTC_ERROR");
+			sb.append("FAVOR_UASTC_ERROR");
 		}
-		if ((n & KTX_PACK_UASTC_FAVOR_BC7_ERROR) != 0) {
+		if ((n & FAVOR_BC7_ERROR) != 0) {
 			if (sb.length() != 0) {
 				sb.append("|");
 			}
-			sb.append("KTX_PACK_UASTC_FAVOR_BC7_ERROR");
+			sb.append("FAVOR_BC7_ERROR");
 		}
-		if ((n & KTX_PACK_UASTC_ETC1_FASTER_HINTS) != 0) {
+		if ((n & ETC1_FASTER_HINTS) != 0) {
 			if (sb.length() != 0) {
 				sb.append("|");
 			}
-			sb.append("KTX_PACK_UASTC_ETC1_FASTER_HINTS");
+			sb.append("ETC1_FASTER_HINTS");
 		}
-		if ((n & KTX_PACK_UASTC_ETC1_FASTEST_HINTS) != 0) {
+		if ((n & ETC1_FASTEST_HINTS) != 0) {
 			if (sb.length() != 0) {
 				sb.append("|");
 			}
-			sb.append("KTX_PACK_UASTC_ETC1_FASTEST_HINTS");
+			sb.append("ETC1_FASTEST_HINTS");
 		}
-		if ((n & KTX_PACK_UASTC_ETC1_DISABLE_FLIP_AND_INDIVIDUAL) != 0) {
+		if ((n & ETC1_DISABLE_FLIP_AND_INDIVIDUAL) != 0) {
 			if (sb.length() != 0) {
 				sb.append("|");
 			}
-			sb.append("KTX_PACK_UASTC_ETC1_DISABLE_FLIP_AND_INDIVIDUAL");
+			sb.append("ETC1_DISABLE_FLIP_AND_INDIVIDUAL");
 		}
 		return sb.toString();
 	}
