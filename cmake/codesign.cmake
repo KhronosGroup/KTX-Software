06d66b67051d5be7d80e119b93626183f8ddfc49
Mark Callow <2244683+MarkCallow@users.noreply.github.com>
2024-09-08 09:30:23 +0900

    Fix linux rpath (#941)
    Add $ORIGIN to RUNPATH for Linux apps and fix settings so
    `CMAKE_BUILD_WITH_INSTALL_RPATH` is set for Linux.

--- cmake/codesign.cmake
+++ cmake/codesign.cmake
@@ -9,9 +9,6 @@ macro (set_code_sign target)
       XCODE_ATTRIBUTE_DEVELOPMENT_TEAM "${XCODE_DEVELOPMENT_TEAM}"
       XCODE_ATTRIBUTE_OTHER_CODE_SIGN_FLAGS "--timestamp"
       XCODE_ATTRIBUTE_CODE_SIGN_INJECT_BASE_ENTITLEMENTS $<IF:$<CONFIG:Debug>,YES,NO>
-      # Necessary for working code signing. Signing happens at build time
-      # so the binary cannot be altered during install.
-      BUILD_WITH_INSTALL_RPATH ON
     )
     if(IOS)
       set(set_pps FALSE)
