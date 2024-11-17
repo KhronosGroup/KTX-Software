03a4c6849001cc825d5d5ba664cfa39c7f780a9e
Wasim Abbas <wasim.abbas@arm.com>
2024-11-17 23:11:12 +0000

    Update format help text

--- tools/ktx/command_encode.cpp
+++ tools/ktx/command_encode.cpp
@@ -146,7 +146,7 @@ void CommandEncode::OptionsEncode::init(cxxopts::Options& opts) {
         (kFormat, "KTX format enum that specifies the KTX file output format."
                   " The enum names are matching the VkFormats without the VK_FORMAT_ prefix."
                   " The VK_FORMAT_ prefix is ignored if present."
-                  "\nIt can't be used with --codec and is only valid for ASTC encoding."
+                  "\nIt can't be used with --codec."
                   "\nThe value must be an ASTC format. When specified the ASTC encoder specific"
                   " options becomes valid."
                   " Case insensitive.", cxxopts::value<std::string>(), "<enum>")
