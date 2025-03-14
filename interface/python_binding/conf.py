a45ffed4d1eb27ee939f008d940d6288b9565b0a
Mark Callow <2244683+MarkCallow@users.noreply.github.com>
2025-03-14 20:51:07 +0900

    Set up workflow to build just the documentation. (#989)
    The plan is to use this workflow to build the docs for PRs and pushes to
    main. Due to the tight deadline for 4.4, there is no time to update the
    per-platform software build workflows to only build the docs only when
    building for a release so only the Ubuntu build is enabled now. This is
    used to deploy the docs to GitHub Pages. For now this only happens for a
    release tag. Once a location is set-up the plan is to deploy docs for
    release tags to somewhere on khronos.org and deploy docs to GitHub Pages
    on any pushes to main.
    
    The following other changes are incorporated in this PR:
    
    - Make a separate `pyktx.doc` target instead of bolting the commands to
      build the documents on to the `pyktx` target.
    - Fix CMake 3.31 warnings in the pyktx CMakeLists.txt.
    - Update copyright, author and e-mail address in the pyktx package and
      documentation.
    - Add lists of files to ignore for in each workflow file.

--- interface/python_binding/conf.py
+++ interface/python_binding/conf.py
@@ -12,13 +12,25 @@
 import os
 import sys
 
+# NOTA BENE
+# The compiled modules that autodoc is trying to load are in
+# <ktx-source>/interface/python_binding but when autodoc is run it is
+# given the build directory as its "source" directory so it uses the copy
+# of this file in the build directory. Therefore the following 3 lines add
+# the build directory to sys.path which does not help autodoc find the
+# modules. However something is adding <ktx-source>/interface/python_binding
+# to sys.path. My best guess is that sphinx/autodoc add its working
+# directory which is the source directory.
 current_dir = os.path.dirname(__file__)
 target_dir = os.path.abspath(os.path.join(current_dir, "."))
 sys.path.insert(0, target_dir)
 
+#print("*******" + __file__ + "**********\n", file=sys.stderr)
+#print(sys.path, file=sys.stderr)
+
 project = 'pyktx'
-copyright = '2023, Shukant Pal'
-author = 'Shukant Pal'
+copyright = '2025, Khronos Group, Inc. 2023, Shukant Pal'
+author = 'Shukant Pal, Mark Callow'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
