155ee6e1a1dd8530e47ab1bc8e37365f694f466e
Mark Callow <github@callow.im>
2025-06-20 13:17:03 +0900

    Insert missing spaces.

--- scripts/build_win.ps1
+++ scripts/build_win.ps1
@@ -95,7 +95,7 @@ if($CMAKE_TOOLSET) {
 }
 # Just setting the environment variable does not seem to work, so pass to cmake.
 if($env:VCPKG_INSTALL_OPTIONS) {
-  cmake_args+=@("-D", "VCPKG_INSTALL_OPTIONS=$env:VCPKG_INSTALL_OPTIONS")
+  cmake_args += @( "-D", "VCPKG_INSTALL_OPTIONS=$env:VCPKG_INSTALL_OPTIONS" )
 }
 if($FEATURE_LOADTESTS -ne "OFF" -and $env:VCPKG_ROOT) {
   $cmake_args += @(
