b0dfa17344c3da5cc4237fce4693d3481be414a3
Mark Callow <github@callow.im>
2025-07-07 14:57:23 +0900

    Prevent spurious multigesture events.
    Make detection of zoomin vs. spreading more robust and process finger up
    events to ensure recogniztion of end of gesture.

--- tests/loadtests/common/LoadTestSample.cpp
+++ tests/loadtests/common/LoadTestSample.cpp
@@ -19,9 +19,28 @@
 #include "LoadTestSample.h"
 #include <SDL3/SDL_log.h>
 
-#if !defined(LOG_GESTURE_DETECTION)
-  #define LOG_GESTURE_DETECTION 0
+#if !defined(LOADTESTSAMPLE_LOG_MOTION_EVENTS)
+  #define LOADTESTSAMPLE_LOG_MOTION_EVENTS 0
 #endif
+#if !defined(LOADTESTSAMPLE_LOG_UP_DOWN_EVENTS)
+  #define LOADTESTSAMPLE_LOG_UP_DOWN_EVENTS 0
+#endif
+#if !defined(LOADTESTSAMPLE_LOG_GESTURE_DETECTION)
+  #define LOADTESTSAMPLE_LOG_GESTURE_DETECTION 0
+#endif
+#if !defined(LOADTESTSAMPLE_LOG_GESTURE_EVENTS)
+  #define LOADTESTSAMPLE_LOG_GESTURE_EVENTS 0
+#endif
+
+[[maybe_unused]] static const char*
+buttonName(Uint8 button) {
+    switch(button) {
+        case SDL_BUTTON_LEFT: return "left";
+        case SDL_BUTTON_MIDDLE: return "middle";
+        case SDL_BUTTON_RIGHT: return "right";
+        default: return "other";
+    }
+}
 
 int
 LoadTestSample::doEvent(SDL_Event* event)
@@ -30,6 +49,9 @@ LoadTestSample::doEvent(SDL_Event* event)
       case SDL_EVENT_MOUSE_MOTION:
       {
         SDL_MouseMotionEvent& motion = event->motion;
+#if LOADTESTSAMPLE_LOG_MOTION_EVENTS
+        SDL_Log("LTS: MOUSE_MOTION - x: %f, y: %f", motion.x, motion.y);
+#endif
         // On macOS with trackpad, SDL_TOUCH_MOUSEID is never set.
         // Prefer mouse events on macOS because press is required. When
         // finger motion events are used the object starts to rotate when
@@ -61,6 +83,10 @@ LoadTestSample::doEvent(SDL_Event* event)
         //if (event->button.which == SDL_TOUCH_MOUSEID)
         //    return 0;
         mousePos = glm::vec2((float)event->button.x, (float)event->button.y);
+        if (LOADTESTSAMPLE_LOG_UP_DOWN_EVENTS) {
+            SDL_Log("LTS: MOUSE_DOWN - button: %s, x: %f, y: %f", buttonName(event->button.button),
+                     event->button.x, event->button.y);
+        }
         switch (event->button.button) {
           case SDL_BUTTON_LEFT:
             mouseButtons.left = true;
@@ -81,6 +107,10 @@ LoadTestSample::doEvent(SDL_Event* event)
         //    return 0;
         //if (event->button.which == SDL_TOUCH_MOUSEID)
         //    return 0;
+        if (LOADTESTSAMPLE_LOG_UP_DOWN_EVENTS) {
+            SDL_Log("LTS: MOUSE_UP - button: %s, x: %f, y: %f", buttonName(event->button.button),
+                     event->button.x, event->button.y);
+        }
         switch (event->button.button) {
           case SDL_BUTTON_LEFT:
             mouseButtons.left = false;
@@ -95,30 +125,65 @@ LoadTestSample::doEvent(SDL_Event* event)
             return 1;
         }
         return 0;
+      // N.B. On macOS (at least) finger down and finger up events are received when
+      // fingers enter and leave the window.
       case SDL_EVENT_FINGER_DOWN: {
+        // Prevent multifingers from triggering the left button action and
+        // interfering with multigestures.
+        //
         // On iOS you get a left button down event no matter how many fingers
         // you touch to the screen. We want 1 finger mouse to work so
-        // behaviour is same as pressing the trackpad on macOS, etc. On iOS
-        // buttondown events come before fingerdown. Hope that is true on
-        // other platforms.
+        // behaviour is same as pressing the trackpad on macOS, etc. As iOS
+        // button_down events come before finger_down we can clear the left
+        // button down state, if we have multiple fingers. Hope this ordering
+        // is the same on other touch screen platforms that send a left-button
+        // event regardless of the number of fingers.
+        //
+        // On macOS button_down events come after finger_down so this code has
+        // no effect.
         //
         // Another way to handle this is to identify the platform and work
         // differently for each platform.
-        //
-        // Prevent multifingers from triggering the left button action and
-        // interfering with multigestures.
         int numFingers;
         SDL_Finger** fingers = SDL_GetTouchFingers(event->tfinger.touchID, &numFingers);
-        if (numFingers > 1)
+        if (LOADTESTSAMPLE_LOG_UP_DOWN_EVENTS) {
+            SDL_Log("LTS: Finger: %" SDL_PRIs64 " down - fingers: %i, x: %f, y: %f",
+                    event->tfinger.fingerID, numFingers, event->tfinger.x, event->tfinger.y);
+        }
+        if (numFingers > 1) {
             mouseButtons.left = false;
-        SDL_free(fingers);
+            if (LOADTESTSAMPLE_LOG_UP_DOWN_EVENTS) {
+                SDL_Log("LTS: FINGER_DOWN with multiple fingers received."
+                        " Resetting mouseButtons.left.");
+            }
+        }
+        // It is possible to somehow get out of the window without seeing FINGER_UP so
+        // as a safeguard stop any previous gesture.
         accumDist = accumTheta = 0;
         zooming = rotating = false;
+        SDL_free(fingers);
         return 0;
       }
+      case SDL_EVENT_FINGER_UP: {
+        int numFingers;
+        SDL_Finger** fingers = SDL_GetTouchFingers(event->tfinger.touchID, &numFingers);
+        if (LOADTESTSAMPLE_LOG_UP_DOWN_EVENTS) {
+            SDL_Log("LTS: Finger: %" SDL_PRIs64 " up - fingers: %i, x: %f, y: %f",
+                    event->tfinger.fingerID, numFingers, event->tfinger.x, event->tfinger.y);
+        }
+        if (numFingers == 2) {
+            accumDist = accumTheta = 0;
+            zooming = rotating = false;
+        }
+        SDL_free(fingers);
+        break;
+      }
 #if 0
-      case SDL_FINGERMOTION:
-        if (SDL_GetNumTouchFingers(event->tfinger.touchId) == 1) {
+      case SDL_FINGERMOTION: {
+        int numFingers;
+        SDL_Finger** fingers = SDL_GetTouchFingers(event->tfinger.touchID, &numFingers);
+        SDL_free(fingers);
+        if (numFingers == 1) {
             rotation.x += yflip * event->tfinger.dy * 225.0f;
             rotation.y += event->tfinger.dx * 225.0f;
             viewChanged();
@@ -132,40 +197,48 @@ LoadTestSample::doEvent(SDL_Event* event)
             accumDist += mgesture.dDist;
             accumTheta +=
                     static_cast<float>(mgesture.dTheta * 180.0 / M_PI);
-            if (LOG_GESTURE_DETECTION) {
-                SDL_Log("dDist = %f, accumDist = %f",
+            if (LOADTESTSAMPLE_LOG_GESTURE_EVENTS) {
+                SDL_Log("LTS MG: Not zooming or rotating. dDist = %f, accumDist = %f,"
+                        " dTheta = %f°, accumTheta = %f°",
                         mgesture.dDist,
-                        accumDist);
-                SDL_Log("dTheta = %f°, accumTheta = %f°",
-                        mgesture.dTheta * 180.0 / M_PI, accumTheta);
+                        accumDist,
+                        mgesture.dTheta * 180.0 / M_PI,
+                        accumTheta);
             }
         }
+        // This is all heuristics derived from use.
         if (zooming) {
             zoom += mgesture.dDist * 10.0f;
-        } else {
-            if (fabs(accumDist) > 0.018) {
-                if (LOG_GESTURE_DETECTION) SDL_Log("zooming detected");
+            if (LOADTESTSAMPLE_LOG_GESTURE_EVENTS) {
+                SDL_Log("LTS MG: Zooming. zoom = %f", rotation.z);
+            }
+        } else if (!rotating) {
+            if (fabs(accumDist) > 0.012 && fabs(accumTheta) <= 3.0) {
                 zooming = true;
                 zoom += accumDist * 10.0f;
+                if (LOADTESTSAMPLE_LOG_GESTURE_DETECTION) {
+                    SDL_Log("LTS MG: spreading detected,"
+                            " accumTheta = %f°, accumDist = %f, zoom = %f",
+                            accumTheta, accumDist, zoom);
+                }
             }
         }
         if (rotating) {
             rotation.z +=
                 static_cast<float>(mgesture.dTheta * 180.0 / M_PI);
-            if (LOG_GESTURE_DETECTION) {
-                SDL_Log("rotation.z = %f°", rotation.z);
+            if (LOADTESTSAMPLE_LOG_GESTURE_EVENTS) {
+                SDL_Log("LTS MG: Rotating around Z. rotation.z = %f°", rotation.z);
             }
-       } else {
-            if (fabs(accumTheta) > 20) {
-                if (LOG_GESTURE_DETECTION) {
-                    SDL_Log("rotation detected, accumTheta = %f°, rotation.z = %f°",
-                            accumTheta,
-                            rotation.z);
-                }
+       } else if (!zooming) {
+            if (fabs(accumTheta) > 15.0) {
                 rotating = true;
                 rotation.z += accumTheta;
-                if (LOG_GESTURE_DETECTION) {
-                    SDL_Log("rotation.z = %f°", rotation.z);
+                if (LOADTESTSAMPLE_LOG_GESTURE_DETECTION) {
+                    SDL_Log("LTS MG: rotation detected,"
+                            " accumTheta = %f°, accumDist = %f, rotation.z = %f°",
+                            accumTheta,
+                            accumDist,
+                            rotation.z);
                 }
             }
         }
