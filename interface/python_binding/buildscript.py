00476ec8ac5ddef5c8cc745a1c83f9bdf1972cf8
Mark Callow <github@callow.im>
2025-10-24 11:51:48 +0900

    Remove debug code.

--- interface/python_binding/buildscript.py
+++ interface/python_binding/buildscript.py
@@ -30,10 +30,6 @@ elif os.name == 'posix':
     if LIBKTX_LIB_DIR is None:
         LIBKTX_LIB_DIR = '/usr/local/lib'
 
-#LIBKTX_LIB_DIR = os.path.abspath(LIBKTX_LIB_DIR);
-#if LIBKTX_IMPORT_DIR is not None:
-#    LIBKTX_IMPORT_DIR = os.path.abspath(LIBKTX_IMPORT_DIR);
-
 library_dirs = ([LIBKTX_IMPORT_DIR] if LIBKTX_IMPORT_DIR is not None else [LIBKTX_LIB_DIR] if LIBKTX_LIB_DIR is not None else [])
 print("library_dirs = ", library_dirs)
 
