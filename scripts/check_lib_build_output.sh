34faf3a687a12c6eb42ddcdd7157645f9a031aa6
Mark Callow <github@callow.im>
2025-10-26 19:38:35 +0900

    cd to specified directory before looking for ktx.version.

--- scripts/check_lib_build_output.sh
+++ scripts/check_lib_build_output.sh
@@ -10,7 +10,16 @@ function usage() {
   echo "$0 <directory> where <directory> is the directory to check."
 }
 
-version=$(cat ktx.version)
+cd $1
+# Expect to be in the output of a build created by a multi-config generator.
+version=$(cat ../ktx.version)
+if [[ -z "$version" ]]; then
+  # No file. Perhaps a single config generator was used.
+  version=$(cat ktx.version)
+fi
+if [[ -z "$version" ]]; then
+  echo "ktx.version file is missing from $1/.. and $1."
+fi
 declare -a expected
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   expected=(libktx.so.$version libktx.so.4 libktx.so libktx_read.so.$version libktx_read.so.4 libktx_read.so)
@@ -19,7 +28,6 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 elif [[ "$OSTYPE" == "win32" ]]; then
   expected=(ktx.dll ktx.lib ktx_read.dll ktx_read.lib)
 fi
-cd $1
 for e in ${expected[@]}; do
   if [[ ! -e $e ]]; then
     echo "Expected file $e does not exist"
