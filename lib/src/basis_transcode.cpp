dfbf9561805f1170b28cde2674b270beb079c841
Mark Callow <github@callow.im>
2025-11-01 18:02:16 +0900

    Fix gcc15 and Windows builds.

--- lib/src/basis_transcode.cpp
+++ lib/src/basis_transcode.cpp
@@ -30,7 +30,7 @@
 #include "vk_format.h"
 #include "basis_sgd.h"
 #if defined(__GNUC__) && !defined(__clang__)
-// If Rich does not accept the warning fixes need to add -Wunused-local-typedef for clang here.
+  // If Rich does not accept the warning fixes need to add -Wunused-local-typedef for clang here.
   #pragma GCC diagnostic ignored "-Wunused-value"
 #endif
 #include "transcoder/basisu_transcoder.h"
