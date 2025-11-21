4bf5756c15d64d0b4499aa0dd5f515c77ffb611a
Mark Callow <github@callow.im>
2025-11-21 20:48:40 +0900

    Change options names to match basis_universal.
    BASISU_SUPPORT_* -> BASISU_*. Only display these options if they make
    sense for the platform and CPU.

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -29,7 +29,7 @@ cmake_args=("-G" "Ninja" \
   "-D" "ANDROID_NDK=$ANDROID_NDK" \
   "-D" "CMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake" \
   "-D" "CMAKE_BUILD_TYPE=$CONFIGURATION" \
-  "-D" "BASISU_SUPPORT_SSE=OFF" \
+  "-D" "BASISU_SSE=OFF" \
   "-D" "${ASTCENC_ISA}"
   "-D" "KTX_FEATURE_TESTS=OFF"
   "-D" "KTX_WERROR=$WERROR"
