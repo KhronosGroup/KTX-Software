7f0f889e3c27f49881e34cb30f8144ec142d68bc
Mark Callow <github@callow.im>
2025-11-28 10:24:01 +0900

    Fix use of libktx project as subproject outside of KTX-Software (#1089)
    #### The Fix
    
    * Always include `cmake/codesign.cmake` and `cmake/cputypetest.cmake`.
      Add `include_guard()` to them to prevent multiple inclusion.
    * Add host project for testing sub-project use and update workflow
       to build and run it and libktx.
    
    #### Other Changes
    * Add options to select building of full or read-only libktx.
    * Fix macOS build when CODE_SIGN_IDENTITY not set. Fixes
      both libktx and the larger KTX-Software project.
    
    Fixes #1083

--- cmake/cputypetest.cmake
+++ cmake/cputypetest.cmake
@@ -1,6 +1,8 @@
 # Copyright 2016, Simon Werta (@webmaster128).
 # SPDX-License-Identifier: Apache-2.0
 
+include_guard(DIRECTORY)
+
 set(cputypetest_code "
 //
 // https://gist.github.com/webmaster128/e08067641df1dd784eb195282fd0912f
