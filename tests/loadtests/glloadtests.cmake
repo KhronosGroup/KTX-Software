979d62054d612f2fe059686926253f2c584c66e0
Mark Callow <github@callow.im>
2026-01-01 18:43:16 +0900

    Fix emscripten build of glloadtests.

--- tests/loadtests/glloadtests.cmake
+++ tests/loadtests/glloadtests.cmake
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18bae3a21735c99a6863f00db29b2d5f49c663901343da2315d2b21245e91fdf
-size 16475
+oid sha256:df6b77b153439867d26f5cbc9cb1f8491acec3dfea7cef5cb80dc2ffcb3d4a58
+size 16353
