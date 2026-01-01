19d6c3c9cab30d10ec07419d557f04181e3ddf0c
Mark Callow <github@callow.im>
2026-01-01 16:13:52 +0900

    Add dump of encoder input data for debugging.

--- lib/src/basis_encode.cpp
+++ lib/src/basis_encode.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24bac95723d892673f6a8c52a1a97b695a9a07906230b58c9dd195b44f8eb918
-size 42051
+oid sha256:c376312117a962f7b44f8d859b03fe28e528dddcd7eeb2d96a35605690cf91cf
+size 42666
