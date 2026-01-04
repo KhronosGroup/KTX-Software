6bb6b89125ff7236730e6af12f96dfd552c79147
Mark Callow <github@callow.im>
2026-01-04 16:20:32 +0900

    Fix C++20 build issues.
    - Remove OutputString:: prefix from in class definition of constructor.
    - Add u8string overload of InputStream constructor.
    - Fix return type of from_u8string.
    - Add from_u8string and to_u8string overloads that take u8string_view
      and string_view respectively.
    - Provide overloads of fmtInFile and fmtOutFile that take u8string_view.
    - Avoid use of filesystem::path::u8path in c++20 to avoid deprecation
      warning.

--- utils/platform_utils.h
+++ utils/platform_utils.h
@@ -121,7 +121,13 @@ inline int unlinkUTF8(const std::string& path) {
     inline std::string from_u8string(const std::u8string& s) {
         return std::string(s.begin(), s.end());
     }
-    inline std::string to_u8string(const std::string& s) {
+    inline std::string from_u8string(const std::u8string_view& s) {
+        return std::string(s.begin(), s.end());
+    }
+    inline std::u8string to_u8string(const std::string& s) {
+        return std::u8string(s.begin(), s.end());
+    }
+    inline std::u8string to_u8string(const std::string_view& s) {
         return std::u8string(s.begin(), s.end());
     }
 
