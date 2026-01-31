b3619b25bf35207bfac6109079b6dc6e1cd132e8
Marius Elvert <marius.elvert@googlemail.com>
2026-01-31 02:35:57 +0100

    Fix build problems w/ fmt and C++23 (#1102)
    Fixes #1101 .

--- tools/ktx/command_compare.cpp
+++ tools/ktx/command_compare.cpp
@@ -178,7 +178,7 @@ struct DiffHexFixedWidth : public DiffBase<T> {
 
     virtual std::string value(std::size_t index, OutputFormat format) const override {
         if (format == OutputFormat::text) {
-            return fmt::format(fmt::format("0x{{:0{}x}}", sizeof(T) << 1), DiffBase<T>::rawValue(index));
+            return fmt::format("0x{1:0{0}x}", sizeof(T) << 1, DiffBase<T>::rawValue(index));
         } else {
             return fmt::format("{}", DiffBase<T>::rawValue(index));
         }
@@ -499,7 +499,7 @@ class PrintDiff {
             if (!std::exchange(firstContext, false))
                 printIndent(0, "\n");
             for (const auto& ctx : context)
-                printIndent(0, ctx);
+                printIndent(0, fmt::runtime(ctx));
             context.clear();
         }
     }
@@ -648,11 +648,11 @@ public:
         const auto space = outputFormat != OutputFormat::json_mini ?  " " : "";
         const auto nl = outputFormat != OutputFormat::json_mini ?  "\n" : "";
 
-        auto formatOptionalFileOffset = [](const std::optional<std::size_t>& fileOffset, std::size_t imageOffset, bool json) {
+        auto formatOptionalFileOffset = [](const std::optional<std::size_t>& fileOffset, std::size_t imageOffset, bool json) -> std::string {
             if (fileOffset.has_value())
-                return fmt::format(json ? "{}" : "0x{:x}", *fileOffset + imageOffset);
+                return fmt::format(fmt::runtime(json ? "{}" : "0x{:x}"), *fileOffset + imageOffset);
             else
-                return fmt::format(json ? "null" : "N/A");
+                return json ? "null" : "N/A";
         };
 
         auto formatPacked = [=](const ImageSpan::TexelBlockPtr<>& texelBlock, bool json) {
