47345553a97efa390622901e9b9e7a3d3bea362f
Mark Callow <github@callow.im>
2026-03-18 22:48:37 +0900

    Remove prefices from enumeration names.

--- interface/python_binding/pyktx/khr_df_primaries.py
+++ interface/python_binding/pyktx/khr_df_primaries.py
@@ -6,46 +6,46 @@ from enum import IntEnum
 class KhrDfPrimaries(IntEnum):
     """The primary colors of an image."""
 
-    KHR_DF_PRIMARIES_UNSPECIFIED = 0
+    UNSPECIFIED = 0
     """No color primaries defined"""
 
-    KHR_DF_PRIMARIES_BT709       = 1
+    BT709       = 1
     """Color primaries of ITU-R BT.709 and sRGB"""
 
-    KHR_DF_PRIMARIES_SRGB        = 1
-    """Synonym for KHR_DF_PRIMARIES_BT709"""
+    SRGB        = 1
+    """Synonym for BT709"""
 
-    KHR_DF_PRIMARIES_BT601_EBU   = 2
+    BT601_EBU   = 2
     """Color primaries of ITU-R BT.601 (625-line EBU variant)"""
 
-    KHR_DF_PRIMARIES_BT601_SMPTE = 3
+    BT601_SMPTE = 3
     """Color primaries of ITU-R BT.601 (525-line SMPTE C variant)"""
 
-    KHR_DF_PRIMARIES_BT2020      = 4
+    BT2020      = 4
     """Color primaries of ITU-R BT.2020"""
 
-    KHR_DF_PRIMARIES_BT2100      = 4
+    BT2100      = 4
     """ITU-R BT.2100 uses the same primaries as BT.2020"""
 
-    KHR_DF_PRIMARIES_CIEXYZ      = 5
+    CIEXYZ      = 5
     """CIE theoretical color coordinate space"""
 
-    KHR_DF_PRIMARIES_ACES        = 6
+    ACES        = 6
     """Academy Color Encoding System primaries"""
 
-    KHR_DF_PRIMARIES_ACESCC      = 7
+    ACESCC      = 7
     """Color primaries of ACEScc"""
 
-    KHR_DF_PRIMARIES_NTSC1953    = 8
+    NTSC1953    = 8
     """Legacy NTSC 1953 primaries"""
 
-    KHR_DF_PRIMARIES_PAL525      = 9
+    PAL525      = 9
     """Legacy PAL 525-line primaries"""
 
-    KHR_DF_PRIMARIES_DISPLAYP3   = 10
+    DISPLAYP3   = 10
     """Color primaries of Display P3"""
 
-    KHR_DF_PRIMARIES_ADOBERGB    = 11
+    ADOBERGB    = 11
     """Color primaries of Adobe RGB (1998)"""
 
-    KHR_DF_PRIMARIES_MAX         = 0xFF
+    MAX         = 0xFF
