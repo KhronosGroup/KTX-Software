9b042a2d75af305233027975ba340f8ad9bbdabe
Baktash Abdollah-shamshir-saz <baktash@toomuchvoltage.com>
2026-05-24 18:33:59 -0400

    Transfer queue support

--- lib/src/vkloader.c
+++ lib/src/vkloader.c
@@ -1870,7 +1870,7 @@ setImageLayout(
                     = VK_ACCESS_HOST_WRITE_BIT | VK_ACCESS_TRANSFER_WRITE_BIT;
         }
         imageMemoryBarrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
-        destStageFlags = VK_PIPELINE_STAGE_ALL_GRAPHICS_BIT | VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
+        destStageFlags = VK_PIPELINE_STAGE_ALL_COMMANDS_BIT;
         break;
 
     default:
