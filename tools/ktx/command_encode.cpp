4e0d1835879c8e56781a9446980448935b94467a
Walid Chtioui <walid.chtioui.main@gmail.com>
2026-06-16 11:16:40 +0200

    Record BCn options in ktx encode
    Signed-off-by: Walid Chtioui <walid.chtioui.main@gmail.com>

--- tools/ktx/command_encode.cpp
+++ tools/ktx/command_encode.cpp
@@ -375,7 +375,7 @@ void CommandEncode::executeEncode() {
     }
 
     // Add KTXwriterScParams metadata
-    const auto writerScParams = fmt::format("{}{}{}", options.codecOptions, options.commonOptions, options.compressOptions);
+    const auto writerScParams = fmt::format("{}{}{}{}", options.bcnOptions, options.codecOptions, options.commonOptions, options.compressOptions);
     ktxHashList_DeleteKVPair(&texture->kvDataHead, KTX_WRITER_SCPARAMS_KEY);
     if (writerScParams.size() > 0) {
         // Options always contain a leading space
