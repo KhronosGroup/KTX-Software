cdecd96d8626d70236651296b5f720733566686b
Walid Chtioui <walid.chtioui.main@gmail.com>
2026-07-17 21:52:30 +0200

    Replace `nproc` with equivalent on MacOS and iOS
    Signed-off-by: Walid Chtioui <walid.chtioui.main@gmail.com>

--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -113,7 +113,8 @@ echo ${config_display%??}
 
 # To be supplied as `-j $njobs`. We might add `+ 1` if the particular cmd is IO
 # bound (this is done in a lot of CIs). On GH CIs, this is most likely to be 4.
-njobs=$(nproc)
+# Equivalent of `nproc` on MacOS is `sysctl -n hw.logicalcpu`
+njobs=$(sysctl -n hw.logicalcpu)
 
 # Print cmake command to be able to verify configuration and replicate locally
 echo "running cmake command (in source directory): cmake . ${cmake_args[@]}"
