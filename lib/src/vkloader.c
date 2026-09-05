9bec28983e373480b26ea5f7c48e78a809ec5944
OppositeNor <oppositenormal@outlook.com>
2026-09-05 11:46:37 -0400

    VK_IMAGE_LAYOUT_GENERAL support for image layout
    Since VK_IMAGE_LAYOUT_GENERAL could be used anywhere, we shouldn't assume how it's accessed either before/after the transition. So, we're using the most conservative VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT for both.

--- lib/src/vkloader.c
+++ lib/src/vkloader.c
@@ -1814,6 +1814,12 @@ setImageLayout(
                                 = VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT;
         break;
 
+    case VK_IMAGE_LAYOUT_GENERAL:
+        // Image may be used for arbitrary read/write access.
+        imageMemoryBarrier.dstAccessMask
+                                = VK_ACCESS_MEMORY_READ_BIT | VK_ACCESS_MEMORY_WRITE_BIT;
+        break;
+
     default:
         /* Value not used by callers, so not supported. */
         assert(KTX_FALSE);
